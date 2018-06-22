f76d9982bc3e8290bc88da98918ac09660033fac
sewon <onww1@naver.com>
2018-06-22 13:15:44 +0900

    fixed untracked files

--- push_test2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("push_test 2\n");
-}
--- test4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	int a, b;
-	while(scanf("%d %d", &a, &b)){
-		printf("%d\n", a+b);
-	}
-	return 0;
-}
