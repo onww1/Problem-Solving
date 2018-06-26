6c607a8eb33df4460162ea8216e5250fc8a53368
sewon <onww1@naver.com>
2018-06-26 20:11:24 +0900

    Add Baekjoon Online Judge No.15857 Problem, result: Accept

---
+++ BOJ/boj15857.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int main(int argc, char const *argv[])
+{
+	int choice;
+	scanf("%d", &choice);
+
+	switch(choice)
+	{
+		case 1: printf("a\n"); break;
+		case 2: printf("b\n"); break;
+		case 3: printf("b\n"); break;
+		case 4: printf("c\n"); break;
+		case 5: printf("d\n"); break;
+		case 6: printf("d\n"); break;
+		case 7: printf("d\n"); break;
+		case 8: printf("c\n"); break;
+	}
+
+	return 0;
+}
