b8a608a668ab725f27dfed04668d00819cadb900
Sewon Woo <sewon.dev@gmail.com>
2019-01-16 15:42:42 +0900

    Update file

--- BOJ/problems/boj10835.cpp
+++ BOJ/problems/boj10835.cpp
@@ -1,3 +1,7 @@
+/*
+ *  1st method.
+ */
+
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
@@ -24,27 +28,25 @@ int main(int argc, char *argv[]) {
   return !printf("%d\n", solve(0, 0));
 }
 
+/*
+ *  2nd method.
+ */
+
 // #include <cstdio>
 // #include <algorithm>
-// #define INF 0x7f7f7f7f
 // using namespace std;
 
 // int a[2050], b[2050], dp[2050][2050];
 // int main(int argc, char *argv[]) {
-//   int n, i, j, mx = 0;
+//   int n, i, j;
 //   scanf("%d", &n);
-//   for (i = 1; i <= n; ++i) scanf("%d", a + i);
-//   for (i = 1; i <= n; ++i) scanf("%d", b + i);
-//   b[0] = INF;
-//   for (j = 1; j <= n; ++j) {
-//     for (i = 1; i <= n; ++i) {
-//       if (a[i] > b[j]) dp[i][j] = dp[i][j-1] + b[j];
-//       else {
-//         dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]);
-//         if (a[i] > b[j-1]) dp[i][j] = max(dp[i][j], dp[i][j-1]);
-//       }
-//       mx = max(mx, dp[i][j]);
+//   for (i = n; i > 0; --i) scanf("%d", a + i);
+//   for (i = n; i > 0; --i) scanf("%d", b + i);
+//   for (i = 1; i <= n; ++i) {
+//     for (j = 1; j <= n; ++j) {
+//       dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]);
+//       if (a[i] > b[j]) dp[i][j] = max(dp[i][j], dp[i][j-1] + b[j]);
 //     }
 //   }
-//   return !printf("%d\n", mx);
+//   return !printf("%d\n", dp[n][n]);
 // }
