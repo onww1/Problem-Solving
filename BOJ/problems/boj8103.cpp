2bf6c1549c57b3fdf27256b57aca5212cdfba3df
Sewon Woo <sewon.dev@gmail.com>
2019-01-29 00:22:40 +0900

    Update boj8103.cpp

--- BOJ/problems/boj8103.cpp
+++ BOJ/problems/boj8103.cpp
@@ -34,24 +34,20 @@ int main(int argc, char *argv[]) {
   sort(recs, recs + n, cmp_by_row);
   for (i = 1, j = 0; i <= n; ++i) {
     while (j <= n && recs[j].a == i) pq.push({recs[j].c, recs[j++].idx});
-    if (!pq.empty() && pq.top().first < i) return !printf("NIE\n");
-    if (!pq.empty()) {
-      pos[pq.top().second].first = i;
-      pq.pop();
-    } else return !printf("NIE\n");
+    if (pq.empty() || pq.top().first < i) return !printf("NIE\n");
+    pos[pq.top().second].first = i;
+    pq.pop();
   }
 
   sort(recs, recs + n, cmp_by_col);
   for (i = 1, j = 0; i <= n; ++i) {
     while (j <= n && recs[j].b == i) pq.push({recs[j].d, recs[j++].idx});
-    if (!pq.empty() && pq.top().first < i) return !printf("NIE\n");
-    if (!pq.empty()) {
-      pos[pq.top().second].second = i;
-      pq.pop();
-    } else return !printf("NIE\n");
+    if (pq.empty() || pq.top().first < i) return !printf("NIE\n");
+    pos[pq.top().second].second = i;
+    pq.pop();
   }
 
-  // sort(pos, pos + n);
-  for (i = 0; i < n; ++i) printf("%d %d\n", pos[i].first, pos[i].second);
+  for (i = 0; i < n; ++i) 
+    printf("%d %d\n", pos[i].first, pos[i].second);
   return 0;
 }
