b8f44f660c8c2803b02530e0c3b21b1151e2d3ae
Sewon Woo <sewon.dev@gmail.com>
2019-01-28 12:03:34 +0900

    Add boj1016.cpp

--- BOJ/problems/boj1016.cpp
+++ BOJ/problems/boj1016.cpp
@@ -1,38 +1,17 @@
 #include <cstdio>
-#include <vector>
 using namespace std;
-
 typedef long long ll;
 
+const int MAX = 1000000;
+bool chk[MAX + 1];
+
 int main(int argc, char *argv[]) {
-	bool noPrime[1000001] = {true, true, };
-	ll min, max;
-	scanf("%lld %lld", &min, &max);
-
-	for (int i=2; i*i <= 1000000; ++i) {
-		if (!noPrime[i]) {
-			for (int j=i; i*j <= 1000000; ++j)
-				noPrime[i*j] = true;
+	ll mn, mx, i, j, cnt = 0LL;
+	scanf("%lld %lld", &mn, &mx);
+	for (i = 2; i <= MAX; ++i) {
+		for (j = mn / (i * i); j * i * i <= mx; ++j) {
+			if (j * i * i >= mn && !chk[j * i * i - mn]) ++cnt, chk[j * i * i - mn] = true;
 		}
 	}
-
-	vector<int> prime;
-	for (int i=2; i<1000000; ++i) 
-		if (!noPrime[i]) prime.push_back(i);
-
-	ll cnt = max - min + 1;
-	ll len = max - min + 1;
-	bool isOK;
-	for (ll i = 0LL; i < len; ++i) {
-		isOK = true;
-		for (int j=0; (ll)prime[j] * prime[j] <= min + i; ++j) {
-			if ((min + i) % (prime[j] * prime[j]) == 0) {
-				isOK = false;
-				break;
-			}
-		}
-		if (!isOK) --cnt;
-	}
-	printf("%lld\n", cnt);
-	return 0;
+	return !printf("%lld\n", mx - mn + 1 - cnt);
 }
