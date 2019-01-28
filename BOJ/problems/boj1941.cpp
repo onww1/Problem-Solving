61088860cb47d035890c16fa3126e75f107f378c
Sewon Woo <sewon.dev@gmail.com>
2019-01-28 11:41:22 +0900

    Add boj1941.cpp

--- BOJ/problems/boj1941.cpp
+++ BOJ/problems/boj1941.cpp
@@ -1,27 +1,38 @@
-#include <iostream>
-#include <vector>
+#include <cstdio>
 using namespace std;
 
-void solve(vector< vector<char> >& map, int T, int S, int& cnt) {
-	if (T == 7) {
+const int move_r[] = {-1, 1, 0, 0}, move_c[] = {0, 0, -1, 1};
+char in[5][6];
+bool chk[1 << 25];
+int cnt = 0;
 
+void solve(int T, int scnt, int status) {
+	if (chk[status]) return;
+	chk[status] = true;
+	if (T == 7) {
+		if (scnt >= 4) ++cnt;
+		return;
+	}
+
+	int r, c;
+	for (int i = 0; i < 25; ++i) {
+		if (!(status & (1 << i))) continue;
+		r = i / 5, c = i % 5;
+		for (int k = 0; k < 4; ++k) {
+			int dr = r + move_r[k];
+			int dc = c + move_c[k];
+			if (dr < 0 || dc < 0 || dr >= 5 || dc >= 5 || (status & (1 << (dr * 5 + dc)))) continue;
+			solve(T + 1, scnt + (in[dr][dc] == 'S'? 1 : 0), status | (1 << (dr * 5 + dc)));
+		}
 	}
 }
 
-int main(int argc, char const *argv[])
-{
-	cin.tie(0);
-	ios_base::sync_with_stdio(false);
-
-	vector< vector<char> > map(5);
-	for (int i=0; i<5; i++) {
-		map.resize(5);
-		for (int j=0; j<5; j++) 
-			cin >> map[i][j];
+int main(int argc, char *argv[]) {
+	for (int i = 0; i < 5; ++i) scanf(" %s", in[i]);
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 5; ++j) {
+			solve(1, in[i][j] =='S' ? 1 : 0, 1 << (i * 5 + j));
+		}
 	}
-
-	int cnt = 0;
-	solve(map, 0, 0, cnt);
-	cout << cnt << '\n';
-	return 0;
+	return !printf("%d\n", cnt);
 }
