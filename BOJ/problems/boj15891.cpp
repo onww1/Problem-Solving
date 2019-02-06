957b39852d815a793b15ef7bad4afd48a911c67c
Sewon Woo <sewon.dev@gmail.com>
2019-02-06 19:44:22 +0900

    Add boj15891.cpp

---
+++ BOJ/problems/boj15891.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+int main(int argc, char *argv[]) {
+  int n;
+  scanf("%d", &n);
+  if (n == 1) {
+    printf("43\n");
+    return 1;
+  } else if (n == 2) {
+    printf("5\n");
+    return 2;
+  } else if (n == 3) {
+    printf("3\n");
+    return 3;
+  } else if (n == 4) {
+    printf("23\n");
+    return 4;
+  } else if (n == 5) {
+    printf("16\n");
+    
+  }
+  return 0;
+}
+
+// 스타트링크 사무실에 있는 가장 큰 TV는 X인치다.
+// 스타트링크 사무실 벽에 붙어있는 포스터는 X장이다.
+// 스타트링크 사무실 안에 있는 iPad의 개수는 X개이다. (모든 iPad 시리즈 전부 포함)
+// 스타트링크 사무실 안에 있는 모니터의 개수는 X개이다.
+// 스타트링크 사무실에 있는 게임용 컴퓨터의 RAM의 총 용량은 X GB이다.
