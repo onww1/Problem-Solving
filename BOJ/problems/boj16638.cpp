cf236b07cd1360148c7bbff6674525cfa0c74e6a
Sewon Woo <sewon.dev@gmail.com>
2019-02-09 18:10:01 +0900

    Add boj16638.cpp

--- BOJ/problems/boj16638.cpp
+++ BOJ/problems/boj16638.cpp
@@ -1,69 +1,67 @@
-#include <iostream>
-#include <string>
+#include <cstdio>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int cal(int a, char op, int b) {
-	if (op == '+') return a + b;
-	else if (op == '-') return a - b;
-	else if (op == '*') return a * b;
-	else return a / b;
+vector <char> op;
+vector <int> num;
+
+int cal(int num1, char op, int num2) {
+	if (op == '+') return num1 + num2;
+	else if (op == '-') return num1 - num2;
+	return num1 * num2;
 }
 
-int atoi(char c) {
-	return c-'0';
-}
-
-void solve(vector<int>& num, vector<char>& op, vector<bool>& paren, int& max) {
-	int size = paren.size();
-	
-	if (size == op.size()) {
-		vector<int> stack;
-		stack.push_back(num[0]);
-		for (int i=0; i<size; i++) {
-			if (i+1 < size && paren[i+1]) {
-				int tmp = 
-				if (op[i] == '*')
-			}
-		}
-		max = res > max ? res : max;
+void solve(int visited, int &mx) {
+	if (visited == (1 << op.size()) - 1) {
+		mx = max(mx, num[0]);
 		return;
 	}
 
-	if (!paren.empty() && !paren.back()) {
-		paren.push_back(true);
-		solve(num, op, paren, max);
-		paren.pop_back();
+	for (int i = 0; i < op.size(); ++i) {
+		if ((visited & (1 << i)) == 0 && 
+				(i == 0 || (i > 0 && (visited & (1 << (i-1))) == 0)) && 
+				(i == op.size() - 1 || (i < op.size() - 1 && (visited & (1 << (i+1))) == 0))) {
+			int tmp[2] = {num[i], num[i+1]};
+			num[i] = num[i+1] = cal(num[i], op[i], num[i+1]);
+			for (int j = i-1; j >= 0; --j) if ( visited & (1 << j) ) num[j] = num[i]; else break;
+			for (int j = i; j < op.size(); ++j) if ( visited & (1 << (j + 1)) ) num[j+1] = num[i]; else break;
+			solve(visited | (1 << i), mx);
+			num[i] = tmp[0]; num[i+1] = tmp[1];
+			for (int j = i-1; j >= 0; --j) if ( visited & (1 << j) ) num[j] = tmp[0]; else break;
+			for (int j = i; j < op.size(); ++j) if ( visited & (1 << (j + 1)) ) num[j+1] = tmp[1]; else break;
+		}
 	}
 
-	paren.push_back(false);
-	solve(num,op,paren,max);
-	paren.pop_back();
+	vector <int> nums(num.begin(), num.end());
+	for (int i = 0; i < op.size(); ++i) {
+		if ((visited & (1 << i)) == 0 && op[i] == '*') {
+			nums[i] = nums[i+1] = cal(nums[i], op[i], nums[i+1]);
+			visited |= (1 << i);
+			for (int j = i-1; j >= 0; --j) if ( visited & (1 << j) ) nums[j] = nums[i]; else break;
+			for (int j = i; j < op.size(); ++j) if ( visited & (1 << j) ) nums[j+1] = nums[i]; else break;	
+		}
+	}	
+	for (int i = 0; i < op.size(); ++i) {
+		if ((visited & (1 << i)) == 0) {
+			nums[i] = nums[i+1] = cal(nums[i], op[i], nums[i+1]);
+			visited |= (1 << i);
+			for (int j = i-1; j >= 0; --j) if ( visited & (1 << j) ) nums[j] = nums[i]; else break;
+			for (int j = i; j < op.size(); ++j) if ( visited & (1 << j) ) nums[j+1] = nums[i]; else break;
+		}
+	}
+	mx = max(mx, nums[0]);
 }
 
-int main(int argc, char const *argv[])
-{
-	cin.tie(0);
-	ios_base::sync_with_stdio(false);
+char in[20];
+int main(int argc, char *argv[]) {
+	int n, i, mx = 1 << 31;
+	scanf("%d %s", &n, in);
+	for (i = 0; i < n; ++i) {
+		if (i % 2) op.push_back(in[i]);
+		else num.push_back(in[i] - '0');
+	}	
 
-	int N;
-	cin >> N;
-
-	string str;
-	cin >> str;
-
-	vector<int> num;
-	vector<char> op;
-
-	int len = str.length();
-	for (int i=0; i<len; i++) {
-		if (i % 2) op.push_back(str[i]);
-		else num.push_back(atoi(str[i]));
-	}
-
-	vector<bool> paren;
-	int max = -987654321;
-	solve(num, op, paren, max);
-	cout << max << '\n';
-	return 0;
+	solve(0, mx);
+	return !printf("%d\n", mx);
 }
