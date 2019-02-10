b9b71fb117e033a19f2354980df5791a7d2108b1
Sewon Woo <sewon.dev@gmail.com>
2019-02-10 21:18:48 +0900

    Add boj16156.cpp

--- BOJ/problems/boj16156.cpp
+++ BOJ/problems/boj16156.cpp
@@ -9,6 +9,25 @@ typedef long long ll;
 typedef pair<ll,int> pli;
 typedef pair<pli,int> plii;
 
+const int BS = 1 << 17;
+char buf[BS];
+inline char read() {
+  static int idx = BS;
+  if (idx == BS) {
+    int nidx = fread(buf, 1, BS, stdin);
+    if (!nidx) return 0;
+    idx = 0;
+  }
+  return buf[idx++];
+}
+inline int readInt() {
+  int sum = 0;
+  char now = read();
+  while (now == ' ' || now == '\n') now = read(); 
+  while ('0' <= now && now <= '9') sum = sum * 10 + now - '0', now = read();
+  return sum;
+}
+
 const ll INF = 0x7f7f7f7f7f7f7f7f;
 const int MAX = 500;
 const int move_r[] = {1, 0, -1, 0};
@@ -19,8 +38,8 @@ pli dist[MAX * MAX];
 bool check(int r, int c) { return r >= 0 && r < n && c >= 0 && c < m; }
 
 int main(int argc, char *argv[]) {
-  scanf("%d %d", &n, &m);
-  for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) scanf("%d", &map[i][j]);
+  n = readInt(), m = readInt();
+  for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) map[i][j] = readInt();
   
   if (m == 1) {
     for (int i = 0; i < n; ++i) puts("1");
@@ -30,7 +49,7 @@ int main(int argc, char *argv[]) {
   memset(dist, 0x7f, sizeof(dist));
   priority_queue < plii, vector<plii>, greater<plii> > pq; 
   for (int i = 0; i < n; ++i) {
-    dist[i * m + m - 1] = {0LL, i};
+    dist[i * m + m - 1] = {map[i][m-1], i};
     pq.push({dist[i * m + m - 1], i * m + m - 1});
   }
 
@@ -40,7 +59,7 @@ int main(int argc, char *argv[]) {
     if (dist[cur.Y].X < cur.X.X) continue;
 
     int r = cur.Y / m, c = cur.Y % m;
-    if (c == 0) { cnt[cur.X.Y]++; continue; }
+    if (c == 0) { cnt[cur.X.Y]++; }
 
     for (int k = 0; k < 4; ++k) {
       int dr = r + move_r[k];
