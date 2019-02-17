d798b7db34caaeda55c69c4dc16529f5d443c1fd
Sewon Woo <sewon.dev@gmail.com>
2019-02-17 15:53:28 +0900

    Update boj2618.cpp

--- BOJ/problems/boj2618.cpp
+++ BOJ/problems/boj2618.cpp
@@ -1,3 +1,20 @@
+/*
+ *  BOJ 2618. 경찰차
+ * 
+ *  시간 복잡도 : O(N^2)
+ *  공간 복잡도 : O(N^2)
+ * 
+ *  DP[i][j] : 경찰차 1이 마지막으로 처리한 일이 i번째 일이고,
+ *             경찰차 2가 마지막으로 처리한 일이 j번째 일인 상황에서 
+ *             나머지 일들을 처리하는데 드는 최소 거리의 합 
+ * 
+ *  DP 항을 위와 같이 정의해두고 재귀함수를 돌려서 모든 경우를 확인해주고, 
+ *  그 중에서 최솟값을 얻어내어 출력하도록 했습니다. 
+ * 
+ *  임무를 분배하는 것에 대한 출력은 맨 처음 상황에서부터 1에 주는게 다음 것과 같은지 
+ *  2에 주는게 다음 것과 같은지를 확인함으로써 임무의 분배 순서를 찾아 나갔습니다.
+ */
+
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
@@ -7,7 +24,7 @@ typedef pair<int,int> pii;
 
 const int MAX = 1000;
 const int INF = 0x7f7f7f7f;
-int N, W, DP[MAX + 1][MAX + 1], ans[MAX + 1];
+int N, W, DP[MAX + 1][MAX + 1];
 pii T[MAX + 1], T1, T2;
 
 int dist(pii &pos1, pii &pos2) {
