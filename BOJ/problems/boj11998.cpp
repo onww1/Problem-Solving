647e31d42078308cc6b28f0fa8ca7b535b3559be
Sewon Woo <sewon.dev@gmail.com>
2019-02-28 23:48:21 +0900

    Add boj11998.cpp

---
+++ BOJ/problems/boj11998.cpp
@@ -0,0 +1,112 @@
+#define NON_SUBMIT
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair<int, pii> piii;
+
+// Fast I/O
+const int BUFFER_SIZE = 1 << 18;
+char buf[BUFFER_SIZE];
+inline char read() {
+  static int idx = BUFFER_SIZE;
+  if (idx == BUFFER_SIZE) {
+    int nidx = fread(buf, 1, BUFFER_SIZE, stdin);
+    if (!nidx) return 0;
+    idx = 0;
+  }
+  return buf[idx++];
+}
+inline int readInt() {
+  int sum = 0, flg = 1;
+  char now = read();
+  while (now == ' ' || now == '\n') now = read();
+  if (now == '-') flg = 0, now = read();
+  while ('0' <= now && now <= '9') sum = sum * 10 + now - '0', now = read();
+  return flg ? sum : -sum;
+}
+inline ll readLL() {
+  ll sum = 0, flg = 1;
+  char now = read();
+  while (now == ' ' || now == '\n') now = read();
+  if (now == '-') flg = 0, now = read();
+  while ('0' <= now && now <= '9') sum = sum * 10 + now - '0', now = read();
+  return flg ? sum : -sum;
+}
+
+void open() {
+  TEST(freopen("input.txt", "r", stdin));
+	TEST(freopen("output.txt", "w", stdout));
+	TEST(freopen("debug.txt", "w", stderr));
+}
+
+const int MAX = 200;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int X, Y, K, M, ans;
+int DP[MAX + 5][MAX + 5];
+
+void solve(int x, int y, int r) {
+  if (DP[x][y] > r) return;
+  DP[x][y] = r;
+  ans = min(ans, abs(x + y - M));
+  
+  if (!r) return;
+  solve(X, y, r - 1);
+  solve(x, Y, r - 1);
+  solve(0, y, r - 1);
+  solve(x, 0, r - 1);
+  if (x + y > Y) solve(x + y - Y, Y, r - 1);
+  else solve(0, x + y, r - 1);
+  if (x + y > X) solve(X, x + y - X, r - 1);
+  else solve(x + y, 0, r - 1);
+}
+
+int main(int argc, char *argv[]) {
+  open();
+
+  X = readInt();
+  Y = readInt();
+  K = readInt();
+  M = readInt();
+  ans = M;
+
+  solve(0, 0, K);
+  return !printf("%d\n", ans);
+}
