7caed63ebd3219a0467cc2f4b7a91091341fb928
Sewon Woo <sewon.dev@gmail.com>
2019-03-03 12:26:25 +0900

    Add boj15954.cpp

--- BOJ/problems/boj15954.cpp
+++ BOJ/problems/boj15954.cpp
@@ -1,47 +1,77 @@
-#include <cstdio>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
 #include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
 using namespace std;
 
-int a[500];
-double min = 10000.0;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair<int, pii> piii;
 
-double avg(int start, int end)
-{
-	int sum = 0;
-	for(int i=start; i<=end; i++)
-	{
-		sum += a[i];
-	}
-	return (double)sum / (double)(end-start+1);
+void open() {
+  TEST(freopen("input.txt", "r", stdin));
+	TEST(freopen("output.txt", "w", stdout));
+	TEST(freopen("debug.txt", "w", stderr));
 }
 
-double variance(int start, int end)
-{
-	double m = avg(start, end);
-	double sum = 0;
-	for(int i=start; i<=end; i++)
-	{
-		sum += (((double)a[i] - m) * ((double)a[i] - m)); 
-	}
-	return sum / (double)(end - start + 1);
+const int MAX = 1e5;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int N, K;
+ll sq[501], a[501];
+ldb mn = 1000000.0;
+int main(int argc, char *argv[]) {
+  open();
+  
+  scanf("%d %d", &N, &K);
+  for (int i = 1; i <= N; ++i) {
+    scanf("%lld", a + i);
+    sq[i] = a[i] * a[i];
+    a[i] += a[i - 1];
+    sq[i] += sq[i - 1];
+  }
+
+  for (int i = 0; i <= N - K; ++i) {
+    for (int j = i + K; j <= N; ++j) {
+      ll nu = (sq[j] - sq[i]) * (j - i) - (a[j] - a[i]) * (a[j] - a[i]);
+      ll de = (ll)(j - i) * (j - i);
+      mn = min(mn, (ldb)sqrt((ldb)nu / de));
+    }
+  }
+
+  printf("%.12Lf\n", mn);
+  return 0;
 }
 
-int main(void)
-{
-	int n, k;
-	scanf("%d %d", &n, &k);
-	for(int i=0; i<n; i++)
-		scanf("%d", &a[i]);
-
-	for(int i=k; i<=n; i++)
-	{
-		for(int j=0; j+i-1<n; j++)
-		{
-			double var = variance(j, j+i-1);
-			if(var < min) min = var;
-		}
-	}
-
-	printf("%.11lf\n", sqrt(min));
-	return 0;
-}
+// V = E(X^2) - (E(X))^2
