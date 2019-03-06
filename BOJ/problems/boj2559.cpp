e7dfef20be30d403103bed237004bf0facee6808
Sewon Woo <sewon.dev@gmail.com>
2019-03-06 19:26:26 +0900

    Add boj2559.cpp

---
+++ BOJ/problems/boj2559.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 100000;
+int N, K, A[MAX + 1];
+int main(int argc, char *argv[]) {
+	scanf("%d %d", &N, &K);
+	for (int i = 1; i <= N; ++i) {
+		scanf("%d", &A[i]);
+		A[i] += A[i - 1];
+	}
+
+	int mx = -1e9;
+	for (int i = 0; i + K < N; ++i) 
+		mx = max(mx, A[i + K] - A[i]);
+
+	return !printf("%d\n", mx);
+}
