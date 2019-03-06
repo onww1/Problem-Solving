3d3e59e7a14b14837f02e8ab7d944808783651fe
Sewon Woo <sewon.dev@gmail.com>
2019-03-06 21:58:18 +0900

    Add boj2637.cpp

--- BOJ/problems/boj2637.cpp
+++ BOJ/problems/boj2637.cpp
@@ -10,6 +10,8 @@
  *  그렇게 완제품까지 가면 완제품에는 완제품을 만들 때 필요로 하는 모든 기본 부품의 수가 저장되어 있습니다.
  *
  *  이 때, A[i][j]는 i를 만들 때 필요로 하는 j번 부품의 수입니다.
+ *
+ *  밑에 주석된 코드는 완제품에서 시작해서 거꾸로 내려가는 방식입니다.
  */
 
 #include <algorithm>
@@ -20,6 +22,7 @@
 #define X first
 #define Y second
 using namespace std;
+typedef pair<int,int> pii;
 
 const int MAX = 100;
 int N, M, indegree[MAX + 1], A[MAX + 1][MAX + 1];
@@ -61,45 +64,40 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-/*
-// 왜 안되는지 모르겠는 코드입니다.
+// const int MAX = 100;
+// int N, M, indegree[MAX + 1], A[MAX + 1];
+// bool isNotBasic[MAX + 1];
+// vector <pii> edges[MAX + 1];
 
-const int MAX = 100;
-int N, M, indegree[MAX + 1], A[MAX + 1];
-bool isNotBasic[MAX + 1];
-vector <pii> edges[MAX + 1];
+// int main(int argc, char *argv[]) {
+//     scanf("%d %d", &N, &M);
+//     for (int i = 0, X, Y, K; i < M; ++i) {
+//         scanf("%d %d %d", &X, &Y, &K);
+//         isNotBasic[X] = true;       // 기본 부품이 아니라는 것을 체크
+//         edges[X].push_back({Y, K}); // 자신에게 필요한 부품을 가르킴
+//         indegree[Y]++;              // 자신에게 들어오는 간선의 수를 카운트
+//     }
 
-int main(int argc, char *argv[]) {
-    scanf("%d %d", &N, &M);
-    for (int i = 0, X, Y, K; i < M; ++i) {
-        scanf("%d %d %d", &X, &Y, &K);
-        isNotBasic[X] = true;       // 기본 부품이 아니라는 것을 체크
-        edges[X].push_back({Y, K}); // 자신에게 필요한 부품을 가르킴
-        indegree[Y]++;              // 자신에게 들어오는 간선의 수를 카운트
-    }
+//     queue <int> Q;
+//     A[N] = 1;            // 완제품은 1개만 필요하므로 1로 지정.
+//     for (int i = 1; i <= N; ++i) 
+//         if (!indegree[i]) Q.push(i);
 
-    queue <int> Q;
-    Q.push(N); A[N] = 1;            // 완제품은 1개만 필요하므로 1로 지정.
+//     // 위상정렬
+//     while (!Q.empty()) {
+//         int current = Q.front();
+//         Q.pop();
 
-    // 위상정렬
-    while (!Q.empty()) {
-        int current = Q.front();
-        Q.pop();
+//         for (pii next : edges[current]) {
+//             // 자신이 필요한 갯수에 자신을 만들 때 필요한 다음 부품의 수를 곱해서 다음 부품에 더해줌.
+//             A[next.X] += A[current] * next.Y;
+//             indegree[next.X]--;
+//             if (!indegree[next.X]) Q.push(next.X);
+//         }
+//     }
 
-        for (pii next : edges[current]) {
-            // 자신이 필요한 갯수에 자신을 만들 때 필요한 다음 부품의 수를 곱해서 다음 부품에 더해줌.
-            A[next.X] += A[current] * next.Y;
-            indegree[next.X]--;
-            if (!indegree[next.X]) Q.push(next.X);
-        }
+//     for (int i = 1; i <= N; ++i) 
+//         if (!isNotBasic[i] && A[i]) printf("%d %d\n", i, A[i]);
 
-        for (int i = 1; i <= N; ++i) printf("%d ", A[i]);
-        puts("");
-    }
-
-    for (int i = 1; i <= N; ++i) 
-        if (!isNotBasic[i] && A[i]) printf("%d %d\n", i, A[i]);
-
-    return 0;
-}
-*/
+//     return 0;
+// }
