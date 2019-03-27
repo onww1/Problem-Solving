7acffba54439077a7b5c6fb90f26771a3d52e494
Sewon Woo <sewon.dev@gmail.com>
2019-03-27 21:03:31 +0900

    Add boj8320.cpp

--- BOJ/problems/boj8320.cpp
+++ BOJ/problems/boj8320.cpp
@@ -1,40 +1,15 @@
-#include <iostream>
+#include <cstdio>
 using namespace std;
 
-int cal(int n)
-{
-	int num = 1;
-	int div = 2;
-	int cnt = 0;
-	while(n > 1)
-	{
-		if(n%div == 0)
-		{
-			cnt++;
-			n/=div;
-		}
-		else
-		{
-			num *= (cnt+1);
-			cnt = 0;
-			div++;
-		}
-	}
-
-	return (num+1)/2;
-}
-
-int main(int argc, char const *argv[])
-{
-	int n;
-	cin >> n;
-
-	int sum = 1;
-	for(int i=2; i<=n; i++)
-	{
-		sum += cal(i);
-	}
-
-	cout << sum << '\n';
-	return 0;
+int main(int argc, char *argv[]) {
+    int N, ans = 0;
+    scanf("%d", &N);
+    for (int i = 1, j; i <= N; ++i) {
+    	for (j = 1; j * j < i; ++j) 
+    		if (i % j == 0) ans++;
+    	if (j * j == i) ans++;
+    } 
+    
+    printf("%d\n", ans);
+    return 0;
 }
