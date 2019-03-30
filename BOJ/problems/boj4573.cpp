ad370709901a41d4669ef4cecf320af48f7f296d
Sewon Woo <sewon.dev@gmail.com>
2019-03-30 14:19:23 +0900

    Add boj4573.cpp

---
+++ BOJ/problems/boj4573.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair<int, pii> piii;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+}
+
+const int MAX = 1e5;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int N, A, B, D;
+ldb mx;
+
+int main(int argc, char *argv[]) {
+    open();
+    
+    for (int tc = 1; 1; tc++) {
+        scanf("%d", &N);
+        if (!N) break;
+
+        mx = 0, D = 0;
+        for (int i = 0; i < N; ++i) {
+            scanf("%d %d", &A, &B);
+            if (mx < (ldb)(A * A) / B) {
+                mx = (ldb)(A * A) / B;
+                D = A;
+            }
+        }
+
+        printf("Menu %d: %d\n", tc, D);
+    }
+
+    return 0;
+}
