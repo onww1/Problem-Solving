dd32d190e2ca45cad1462c9ed64bf83455bff592
Sewon Woo <sewon.dev@gmail.com>
2019-04-05 14:43:15 +0900

    Delete files

--- BOJ/1000-1999/boj1188.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-int _gcd(int a, int b) { return b ? _gcd(b, a % b) : a; }
-int main() {
-	int N, M, G;
-	scanf("%d %d", &N, &M);
-	G = _gcd(N, M);
-	N /= G, M /= G;
-	return !printf("%d\n", (M - 1) * G);
-}
--- BOJ/1000-1999/boj1297.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include <cmath>
-
-int main(int argc, char *argv[]) {
-  int d, h, w;
-  scanf("%d %d %d", &d, &h, &w);
-  double ratio = sqrt((double)d*d / (h*h + w*w));
-  return !printf("%d %d\n", (int)(floor(ratio * h)), (int)(floor(ratio * w)));
-}
