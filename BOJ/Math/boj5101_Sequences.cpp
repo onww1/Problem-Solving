614f33588afff887be3e4d9a831031d7503268eb
Sewon Woo <sewon.dev@gmail.com>
2019-04-10 16:08:58 +0900

    boj5101 : Sequences (https://boj.kr/5101)

---
+++ BOJ/Math/boj5101_Sequences.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair<int, pii> piii;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+}
+
+const int MAX = 1e5;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int main(int argc, char *argv[]) {
+    open();
+    
+    int A, B, C;
+    while (1) {
+        scanf ("%d %d %d", &A, &B, &C);
+        if (!A && !B && !C) break;
+        if (B < 0) A *= -1, B *= -1, C *= -1;
+        if ((C - A) % B == 0) printf("%d\n", (C - A) / B + 1);
+        else puts("X");
+    }
+
+    return 0;
+}
