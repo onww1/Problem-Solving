9d8c7a2766e9c2fe151077836b16e1921351b658
Sewon Woo <sewon.dev@gmail.com>
2019-04-12 20:05:15 +0900

    boj1166 : 선물 (https://boj.kr/1166)

--- BOJ/Binary-search/boj1166_선물.cpp
+++ BOJ/Binary-search/boj1166_선물.cpp
@@ -1,15 +1,18 @@
+#include <algorithm>
 #include <cstdio>
+using namespace std;
+typedef long long ll;
 typedef long double ldb;
 
 const ldb ERR = 1e-10;
-int N, L, W, H;
+ll N, L, W, H;
 
-int main() {
-    scanf("%d %d %d %d\n", &N, &L, &W, &H);
-    ldb left = 0.0, right = 1e9, mid, ans;
-    while (right - left > ERR) {
+int main(int argc, char *argv[]) {
+    scanf("%lld %lld %lld %lld\n", &N, &L, &W, &H);
+    ldb left = 0.0, right = 1e9, mid = 1e9, ans = 1e9;
+    while (abs(right - left) > ERR) {
         mid = (left + right) / 2;
-        int cnt = (int)(L / mid) * (int)(W / mid) * (int)(H / mid);
+        ll cnt = (ll)(L / mid) * (ll)(W / mid) * (ll)(H / mid);
         if (cnt >= N) {
             ans = mid;
             left = mid;
