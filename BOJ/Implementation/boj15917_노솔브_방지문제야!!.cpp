dccdd4d304fdccc468088c385398f1c38e0b6c8b
Sewon Woo <sewon.dev@gmail.com>
2019-04-13 23:02:12 +0900

    boj15917 : 노솔브 방지문제야git add BOJ/Implementation/boj15917_노솔브_방지문제야!!.cpp (https://boj.kr/15917)

---
+++ BOJ/Implementation/boj15917_노솔브_방지문제야!!.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+int Q, A;
+
+int main(int argc, char *argv[]) {
+    scanf("%d", &Q);
+    while (Q--) {
+        scanf("%d", &A);
+        printf("%d\n", A & (A - 1) ? 0 : 1);
+    }
+    return 0;
+}
