aeba959ba3c10e66af6dea8b0f80e4f2b44391dd
Sewon Woo <sewon.dev@gmail.com>
2019-05-29 13:17:10 +0900

    boj11977 : Angry Cows (Bronze) (https://boj.kr/11977)

--- BOJ/Brute-force/boj11977_Angry_cows_(Bronze).cpp
+++ BOJ/Brute-force/boj11977_Angry_cows_(Bronze).cpp
@@ -1,41 +1,40 @@
-#include <iostream>
 #include <algorithm>
+#include <cstdio>
 using namespace std;
 
-int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
+int N, A[100];
 
-	int N, A[100];
-	cin >> N;
-	for (int i=0; i<N; i++)
-		cin >> A[i];
+int main(int argc, char *argv[]) {
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i) scanf("%d", A + i);
+    sort(A, A + N);
 
-	sort(A, A+N);
+    int ans = 0;
+    for (int i = 0; i < N; ++i) {
+        int cnt = 1;
+        // left
+        for (int j = i - 1, r = 1, pos = A[i]; j >= 0; r++) {
+            bool changed = false;
+            while (j >= 0 && A[j] + r >= pos) {
+                changed = true;
+                cnt++; j--;
+            }
+            if (!changed) break;
+            else pos = A[j + 1];
+        }
+        // right
+        for (int j = i + 1, r = 1, pos = A[i]; j < N; r++) {
+            bool changed = false;
+            while (j < N && A[j] - r <= pos) {
+                changed = true;
+                cnt++; j++;
+            }
+            if (!changed) break;
+            else pos = A[j - 1];
+        }
+        ans = max(ans, cnt);
+    }
 
-	int max = 0, t = 1, cnt = 0, ptr = 0;
-	for (int i=0; i<N; i++) {
-		cnt = 1;
-		
-		for (ptr = i, t = 1; ptr > 0; t++) {
-			int cur = A[ptr];
-			if (A[ptr-1] + t < cur) break;
-			while (ptr > 0 && A[ptr-1] + t >= cur) {
-				cnt++; ptr--;
-			}
-		}
-
-		for (ptr = i, t = 1; ptr < N-1; t++) {
-			int cur = A[ptr];
-			if (cur + t < A[ptr+1]) break;
-			while (ptr < N-1 && cur + t >= A[ptr+1]) {
-				cnt++; ptr++;
-			}
-		}
-
-		max = max > cnt ? max : cnt;
-	}
-
-	cout << max << '\n';
-	return 0;
+    printf("%d\n", ans);
+    return 0;
 }
