c0a9522bb87ba4bc038818ce45b6ac3cc419e8dd
Sewon Woo <sewon.dev@gmail.com>
2019-05-29 13:28:33 +0900

    boj11978 : Mowing the Field (Bronze) (https://boj.kr/11978)

--- BOJ/Simulation/boj11978_Mowing_the_Field_(Bronze).cpp
+++ BOJ/Simulation/boj11978_Mowing_the_Field_(Bronze).cpp
@@ -1,73 +1,34 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <vector>
-#include <queue>
-#include <map>
 #include <algorithm>
-#define MOD 1000000007
-#define INF 1023456789
+#include <cstdio>
 using namespace std;
 
-typedef long long ll;
-typedef double db;
+const int INF = 0x3f3f3f3f;
+const int move_r[] = {-1, 0, 1, 0};
+const int move_c[] = {0, -1, 0, 1};
+int N, ans = INF, s, MAP[2001][2001];
+char d;
 
-ll get_pow(ll base, ll exp);
-ll get_min(ll a, ll b);
-ll get_max(ll a, ll b);
-ll get_abs(ll a);
-
-int parseDirection(char c);
+int parse(char c) {
+    if (c == 'N') return 0;
+    else if (c == 'W') return 1;
+    else if (c == 'S') return 2;
+    return 3;
+}
 
 int main(int argc, char *argv[]) {
-	int *map;
-	int n, i, s, dd;
-	int row = 1000, col = 1000, min = INF, t = 0;
-	int move_r[] = {0, 0, 1, -1};
-	int move_c[] = {1, -1, 0, 0};
-	char d;
-
-	map = (int *)calloc(2001 * 2001, sizeof(int));
-	scanf("%d", &n);
-	while (n--) {
-		scanf(" %c %d", &d, &s);
-		dd = parseDirection(d);
-		for (i = 0; i < s; ++i) {
-			row += move_r[dd];
-			col += move_c[dd];
-			++t;
-
-			if (*(map + row * 2001 + col)) 
-				min = get_min(min, t - *(map + row * 2001 + col));
-			*(map + row * 2001 + col) = t;
-		}
-	}
-
-	free(map);
-	if (min == INF) printf("-1\n");
-	else printf("%d\n", min);
-  return 0;
+    scanf("%d", &N);
+    MAP[1000][1000] = 1;
+    for (int i = 0, x = 1000, y = 1000, t = 1; i < N; ++i) {
+        scanf(" %c %d", &d, &s);
+        int idx = parse(d);
+        for (int j = 0; j < s; ++j) {
+            ++t;
+            y += move_r[idx];
+            x += move_c[idx];
+            if (MAP[y][x]) ans = min(ans, t - MAP[y][x]);
+            MAP[y][x] = t;
+        }
+    }
+    printf("%d\n", ans == INF ? -1 : ans);
+    return 0;
 }
-
-int parseDirection(char c) {
-	if (c == 'E') return 0;
-	else if (c == 'W') return 1;
-	else if (c == 'S') return 2;
-	else if (c == 'N') return 3;
-	return -1;
-}
-
-ll get_pow(ll base, ll exp) {
-  if (exp == 0ll) return 1ll;
-  if (exp == 1ll) return base;
-
-  ll half = get_pow(base, exp/2ll);
-  ll res = half * half % MOD;
-  if (exp % 2ll) res = res * base % MOD;
-  return res;
-}
-
-ll get_min(ll a, ll b) { return a < b ? a : b; }
-ll get_max(ll a, ll b) { return a > b ? a : b; }
-ll get_abs(ll a) { return a < 0 ? -a : a; }
