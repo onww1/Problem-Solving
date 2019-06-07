9eedb74f56db4ed5dc632e2491cd3b76a1420a37
Sewon Woo <sewon.dev@gmail.com>
2019-06-07 23:27:45 +0900

    boj14444 : 가장 긴 팰린드롬 부분 문자열 (https://boj.kr/14444)

---
+++ BOJ/Manacher/boj14444_가장_긴_팰린드롬_부분_문자열.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+
+const int MAX = 1e5 + 1;
+int N, A[2 * MAX];
+char in[MAX], S[2 * MAX];
+
+int main(int argc, char *argv[]) {
+    scanf(" %s", in); S[0] = '#';
+    for (int i = 0; in[i]; ++i, ++N) {
+        S[2 * i + 1] = in[i];
+        S[2 * i + 2] = '#';
+    }
+    N = 2 * N + 1;
+
+    int r = 0, p = 0;
+    for (int i = 0; i < N; ++i) {
+        if (i <= r) A[i] = min(r - i, A[2 * p - i]);
+        else A[i] = 0;
+        while (i - A[i] - 1 >= 0 && i + A[i] + 1 < N && S[i - A[i] - 1] == S[i + A[i] + 1]) A[i]++;
+        if (i + A[i] > r) {
+            r = i + A[i];
+            p = i;
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < N; ++i) ans = max(ans, A[i]);
+    printf("%d\n", ans);
+    return 0;
+}
