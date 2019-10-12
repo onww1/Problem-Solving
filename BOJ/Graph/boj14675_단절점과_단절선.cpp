b5486aa4d7584b0e7db8fbf9bfbef5331ebb4fa8
Sewon Woo <sewon.dev@gmail.com>
2019-10-13 08:50:16 +0900

    boj14675 : 단절점과 단절선 (https://boj.kr/14675)

---
+++ BOJ/Graph/boj14675_단절점과_단절선.cpp
@@ -0,0 +1,88 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, q, t, k;
+vector <int> edges[MAX];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &n);
+    for (int i = 1, a, b; i < n; ++i) {
+    	scanf("%d %d", &a, &b);
+    	edges[a].push_back(b);
+    	edges[b].push_back(a);
+    }
+
+    scanf("%d", &q);
+    for (int i = 0; i < q; ++i) {
+    	scanf("%d %d", &t, &k);
+    	if (t == 2) puts("yes");
+    	else puts(edges[k].size() > 1 ? "yes" : "no");
+    }
+
+    close();
+    return 0;
+}
