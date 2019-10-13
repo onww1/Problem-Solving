d1b01131fd96569c6c674a0e65a0a847cf700eea
Sewon Woo <sewon.dev@gmail.com>
2019-10-13 17:01:06 +0900

    boj17611 : 직각다각형 (https://boj.kr/17611)

---
+++ BOJ/Sweeping/boj17611_직각다각형.cpp
@@ -0,0 +1,101 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n;
+pii p[MAX];
+vector <pii> lines[2];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) scanf("%d %d", &p[i].X, &p[i].Y);
+
+    if (p[0].X == p[n - 1].X) lines[1].push_back({min(p[0].Y, p[n - 1].Y), max(p[0].Y, p[n - 1].Y)});
+    else lines[0].push_back({min(p[0].X, p[n - 1].X), max(p[0].X, p[n - 1].X)});
+
+    for (int i = 0; i < n - 1; ++i) {
+        if (p[i].X == p[i + 1].X) lines[1].push_back({min(p[i].Y, p[i + 1].Y), max(p[i].Y, p[i + 1].Y)});
+        else lines[0].push_back({min(p[i].X, p[i + 1].X), max(p[i].X, p[i + 1].X)});
+    }
+
+    sort(lines[0].begin(), lines[0].end());
+    sort(lines[1].begin(), lines[1].end());
+
+    int ans = 0;
+    for (int i = 0; i < 2; ++i) {
+        priority_queue <int, vector<int>, greater<int>> pq;
+        for (pii& line : lines[i]) {
+            if (!pq.empty() && pq.top() <= line.X) pq.pop();
+            if (line.X != line.Y) pq.push(line.Y);
+            ans = max(ans, (int)pq.size());
+        }
+    }
+
+    printf("%d\n", ans);
+
+    close();
+    return 0;
+}
