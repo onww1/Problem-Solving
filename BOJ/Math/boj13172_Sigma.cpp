01e3fc122d6fa59451ddcabbba376ec536f90ebd
Sewon Woo <sewon.dev@gmail.com>
2019-10-19 23:48:22 +0900

    boj13172 : Sigma (https://boj.kr/13172)

---
+++ BOJ/Math/boj13172_Sigma.cpp
@@ -0,0 +1,90 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+ll _pow(ll base, ll exp) {
+	ll ret = 1;
+	while (exp) {
+		if (exp % 2LL) ret = ret * base % MOD;
+		base = base * base % MOD;
+		exp /= 2LL;
+	}
+	return ret;
+}
+
+ll m, ans, n, s;
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%lld", &m);
+    while (m--) {
+    	scanf("%lld %lld", &n, &s);
+    	ans = (ans + s * _pow(n, MOD - 2) % MOD) % MOD;
+    }
+    printf("%lld\n", ans);
+
+    close();
+    return 0;
+}
