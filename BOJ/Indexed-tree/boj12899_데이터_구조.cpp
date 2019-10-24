e6e1ce9cfa8de8c24e5fcc61cc6f3f5b35d2cb0a
Sewon Woo <sewon.dev@gmail.com>
2019-10-24 23:11:43 +0900

    boj12899 : 데이터 구조 (https://boj.kr/12899)

---
+++ BOJ/Indexed-tree/boj12899_데이터_구조.cpp
@@ -0,0 +1,105 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int SZ = 1 << 21;
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, tree[2 * SZ];
+
+void update(int node, int val) {
+	node += SZ;
+	while (node) {
+		tree[node] += val;
+		node >>= 1;
+	}
+}
+
+int kth(int k) {
+	int node = 1;
+	while (node < SZ) {
+		if (tree[node * 2] < k) {
+			k -= tree[node * 2];
+			node = node * 2 + 1;
+		}
+		else node *= 2;
+	}
+	return node - SZ;
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &n);
+    for (int i = 0, t, x; i < n; ++i) {
+    	scanf("%d %d", &t, &x);
+    	if (t == 1) update(x, 1);
+    	else {
+    		int kth_elem = kth(x);
+    		update(kth_elem, -1);
+   			printf("%d\n", kth_elem);
+    	} 
+    }
+
+    close();
+    return 0;
+}
