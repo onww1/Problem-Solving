a9a1dcaf4a855cc295cbd4dadb1b5d70be5c8daa
onww1 <onww1@naver.com>
2020-01-05 18:26:34 +0900

    boj18231 : 파괴된 도시 (https://boj.kr/18231)

---
+++ BOJ/Graph/boj18231_파괴된_도시.cpp
@@ -0,0 +1,118 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+bool isDestroyed[2005], isCovered[2005];
+int n, m, k;
+vector <int> edges[2005];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d %d", &n, &m);
+    for (int i = 0, u, v; i < m; ++i) {
+        scanf("%d %d", &u, &v);
+        edges[u].push_back(v);
+        edges[v].push_back(u);
+    }
+    scanf("%d", &k);
+    for (int i = 0, p; i < k; ++i) {
+        scanf("%d", &p);
+        isDestroyed[p] = true;
+    }
+
+    vector <int> ans;
+    bool valid = true;
+
+    for (int i = 1; i <= n; ++i) {
+        if (isDestroyed[i]) {
+            bool bomb = true;
+            for (int next : edges[i]) {
+                if (!isDestroyed[next]) {
+                    bomb = false;
+                    break;
+                }
+            }
+            if (bomb) {
+                ans.push_back(i);
+                isCovered[i] = true;
+                for (int next : edges[i])
+                    isCovered[next] = true;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; ++i)
+        if (isDestroyed[i] ^ isCovered[i]) 
+            valid = false;
+
+    if (valid) {
+        printf("%lu\n", ans.size());
+        for (int node : ans) printf("%d ", node); puts("");
+    }
+    else puts("-1");
+
+    close();
+    return 0;
+}
