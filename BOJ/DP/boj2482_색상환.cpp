d5dd6b682cdd19109f9a0a7b017d2bdc5b55b5bf
onww1 <onww1@naver.com>
2020-06-07 03:05:52 +0900

    boj2482 : 색상환 (https://boj.kr/2482)

---
+++ BOJ/DP/boj2482_색상환.cpp
@@ -0,0 +1,88 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 3;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, k, dp[1001][1001];
+
+int solve(int nn, int kk) {
+    if (nn <= 0) return 0;
+    if (kk == 0) return 1;
+    if (kk == 1) return nn;
+    int &ret = dp[nn][kk];
+    if (ret != -1) return ret;
+    ret = (solve(nn - 2, kk - 1) + solve(nn - 1, kk)) % MOD;
+    return ret;
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d %d", &n, &k);
+    memset(dp, -1, sizeof(dp));
+    int answer = (solve(n - 3, k - 1) + solve(n - 1, k)) % MOD;
+    printf("%d\n", answer);
+
+    close();
+    return 0;
+}
