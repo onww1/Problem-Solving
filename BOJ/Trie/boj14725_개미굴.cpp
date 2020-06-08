2ae7190ad26215bf643a3ef01935ede62aa6984c
onww1 <onww1@naver.com>
2020-06-08 23:04:21 +0900

    boj14725 : 개미굴 (https://boj.kr/14725)

---
+++ BOJ/Trie/boj14725_개미굴.cpp
@@ -0,0 +1,119 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+struct Node {
+    string s;
+    map<string, Node*> children;
+
+    Node(string _s) : s(_s){}
+
+    void insert(queue<string> &q) {
+        if (q.empty()) return;
+        string val = q.front();
+        q.pop();
+        Node* node = children[val];
+        if (node == nullptr) {
+            node = new Node(val);
+            children[val] = node;
+        }
+        node->insert(q);
+    }
+
+    void print(string offset) {
+        cout << offset << s << '\n';
+        offset += "--";
+        for (auto it = children.begin(); it != children.end(); ++it)
+            it->second->print(offset);
+    }
+};
+
+int n, k;
+string ss;
+queue<string> q;
+
+int main(int argc, char *argv[]) {
+    open();
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    Node* root = new Node("");
+
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> k;
+        for (int j = 0; j < k; ++j) {
+            cin >> ss;
+            q.push(ss);
+        }
+        root->insert(q);
+    }
+
+    for (auto it = root->children.begin(); it != root->children.end(); ++it)
+        it->second->print("");    
+
+    close();
+    return 0;
+}
