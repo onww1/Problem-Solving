36e2ada7435f738ad44b8d1e0dbf3f9110fd05e7
onww1 <onww1@naver.com>
2020-06-15 23:15:24 +0900

    boj9935 : 문자열 폭발 (https://boj.kr/9935)

--- BOJ/Stack/boj9935_문자열_폭발.cpp
+++ BOJ/Stack/boj9935_문자열_폭발.cpp
@@ -54,3 +54,109 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("unroll-loops")
+// #include <algorithm>
+// #include <functional>
+// #include <iostream>
+// #include <string>
+// #include <unordered_set>
+// #include <unordered_map>
+// #include <cmath>
+// #include <ctime>
+// #include <cstdio>
+// #include <cstring>
+// #include <cstdlib>
+// #include <vector>
+// #include <queue>
+// #include <deque>
+// #include <stack>
+// #include <map>
+// #include <set>
+// #define X first
+// #define Y second
+
+// #ifdef NON_SUBMIT
+// #define TEST(n) (n)
+// #else
+// #define TEST(n) ((void)0)
+// #endif
+
+// #pragma warning(disable:4996)
+// using namespace std;
+
+// typedef long long ll;
+// typedef unsigned long long ull;
+// typedef double db;
+// typedef long double ldb;
+// typedef pair <int, int> pii;
+// typedef pair <ll, ll> pll;
+// typedef pair <ll, int> pli;
+// typedef pair <int, pii> piii;
+// typedef tuple <int, int, int> ti3;
+
+// clock_t start_time, end_time;
+
+// void open() {
+//     TEST(freopen("input.txt", "r", stdin));
+//     TEST(freopen("output.txt", "w", stdout));
+//     TEST(freopen("debug.txt", "w", stderr));
+//     TEST(start_time = clock());
+// }
+
+// void close() {
+//     TEST(end_time = clock());
+//     TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+// }
+
+// const int MAX = 1e6 + 1;
+// const int MOD = 1e9 + 7;
+// const int INF = 0x3f3f3f3f;
+// const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+// const db PI = acos(-1);
+// const ldb ERR = 1e-10;
+// const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+// const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+// char in[MAX], p[50];
+// int plen;
+// vector<int> mat;
+// vector<char> v;
+
+// int main(int argc, char *argv[]) {
+//     open();
+
+//     scanf(" %s %s", in, p);
+//     for (plen = 0; p[plen]; ++plen);
+//     for (int i = 0, j = 0; in[i]; ++i) {
+//         v.push_back(in[i]);
+//         if (in[i] == p[j]) ++j;
+//         else j = (in[i] == p[0]);
+//         mat.push_back(j);
+//         while (j == plen) {
+//             for (int k = 0; k < plen; ++k) {
+//                 v.pop_back();
+//                 mat.pop_back();
+//             }
+//             if (!mat.empty()) 
+//                 j = mat.back();
+//             else
+//                 j = 0;
+//         }
+//     }
+
+//     if (v.empty()) puts("FRULA");
+//     else {
+//         in[v.size()] = 0;
+//         for (int i = (int)v.size() - 1; i >= 0; --i) {
+//             in[i] = v.back();
+//             v.pop_back();
+//         }
+//         printf("%s\n", in);
+//     }
+
+//     close();
+//     return 0;
+// }
