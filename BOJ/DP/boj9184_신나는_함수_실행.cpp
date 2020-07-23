0300c65c75a693885c6e1bcbc6fcf28dbb69f6a4
onww1 <onww1@naver.com>
2020-07-24 02:17:43 +0900

    boj9184 : 신나는 함수 실행 (https://boj.kr/9184)

--- BOJ/DP/boj9184_신나는_함수_실행.cpp
+++ BOJ/DP/boj9184_신나는_함수_실행.cpp
@@ -1,39 +1,91 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
 using namespace std;
 
-int dp[21][21][21] = { 1, };
-bool visited[21][21][21] = { true, false, };
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
 
-int W(int A, int B, int C) {
-	if(visited[A][B][C]) return dp[A][B][C];
+clock_t start_time, end_time;
 
-	if (A <= 0 || B <= 0 || C <= 0)
-		return dp[0][0][0];
-
-	if (A > 20 || B > 20 || C > 20) {
-		return dp[20][20][20] = W(20, 20, 20);
-	}
-
-	if (A < B && B < C) {
-		visited[A][B][C] = true;
-		return dp[A][B][C] = W(A, B, C-1) + W(A, B-1, C-1) - W(A, B-1, C);
-	}
-
-	visited[A][B][C] = true;
-	return dp[A][B][C] = W(A-1, B, C) + W(A-1, B-1, C) + W(A-1, B, C-1) - W(A-1, B-1, C-1);
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
 }
 
-int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
 
-	int A, B, C;
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
 
-	while( 1 ) {
-		cin >> A >> B >> C;
-		if( A == -1 && B == -1 && C == -1 ) break;
-		cout << "w(" << A << ", " << B << ", " << C << ") = " << W(A, B, C) << '\n';
-	}
+int a, b, c, dp[21][21][21];
 
-	return 0;
+int solve(int x, int y, int z) {
+    if (x <= 0 || y <= 0 || z <= 0) return 1;
+    if (x > 20 || y > 20 || z > 20) x = y = z = 20;
+
+    int& ret = dp[x][y][z];
+    if (ret != INF) return ret;
+    if (x < y && y < z) ret = solve(x, y, z - 1) + solve(x, y - 1, z - 1) - solve(x, y - 1, z);
+    else ret = solve(x - 1, y, z) + solve(x - 1, y - 1, z) + solve(x - 1, y, z - 1) - solve(x - 1, y - 1, z - 1);
+    return ret;
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    memset(dp, 0x3f, sizeof(dp));
+    while (1) {
+        scanf("%d %d %d", &a, &b, &c);
+        if (a == -1 && b == -1 && c == -1) break;
+        printf("w(%d, %d, %d) = %d\n", a, b, c, solve(a, b, c));
+    }
+
+    close();
+    return 0;
 }
