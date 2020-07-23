e4ba552a37e5fceb7c4ad2dd215505c2ce48fc26
onww1 <onww1@naver.com>
2020-07-24 01:58:47 +0900

    boj3079 : 입국심사 (https://boj.kr/3079)

--- BOJ/Binary-search/boj3079_입국심사.cpp
+++ BOJ/Binary-search/boj3079_입국심사.cpp
@@ -1,24 +1,88 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
 #include <cstdio>
-typedef long long ll;
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
 
-const int MAX = 1e5;
-ll N, M, ans, T[MAX];
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, m, t[MAX];
 
 int main(int argc, char *argv[]) {
-    scanf("%lld %lld", &N, &M);
-    for (int i = 0; i < N; ++i) scanf("%lld", T + i);
+    open();
 
-    ll l = 1, r = 1e13, m = 1;
-    while (l <= r) {
-        m = (l + r) / 2;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; ++i) scanf("%d", t + i);
+    ll lo = 0, hi = 1e18;
+    while (lo < hi) {
+        ll mid = (lo + hi + 1) >> 1;
         ll cnt = 0;
-        for (int i = 0; i < N; ++i) cnt += m / T[i];
-        if (cnt >= M) {
-            ans = m;
-            r = m - 1;
-        } else l = m + 1;
+        for (int i = 0; i < n; ++i) {
+            cnt += mid / t[i];
+            if (cnt >= m) break;
+        }
+        if (cnt >= m) hi = mid - 1;
+        else lo = mid;
     }
+    printf("%lld\n", lo + 1);
 
-    printf("%lld\n", ans);
+    close();
     return 0;
 }
