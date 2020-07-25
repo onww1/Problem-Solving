353e939ce65d6c9597b9b68ee87339ef1bd8940f
onww1 <onww1@naver.com>
2020-07-25 16:15:17 +0900

    boj14287 : 내리 칭찬 3 (https://boj.kr/14287)

---
+++ BOJ/Indexed-tree/boj14287_내리_칭찬_3.cpp
@@ -0,0 +1,113 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, m, cnt, pa, tree[MAX], nodes[MAX][2];
+vector<int> edges[MAX];
+
+void dfs(int node) {
+    nodes[node][0] = ++cnt;
+    for (int next: edges[node]) dfs(next);
+    nodes[node][1] = cnt;
+}
+
+void update(int node, int val) {
+    while (node <= n) {
+        tree[node] += val;
+        node += node & -node;
+    }
+}
+
+int sum(int node) {
+    int ret = 0;
+    while (node) {
+        ret += tree[node];
+        node -= node & -node;
+    }
+    return ret;
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d %d %d", &n, &m, &pa);
+    for (int i = 2; i <= n; ++i) {
+        scanf("%d", &pa);
+        edges[pa].push_back(i);
+    }
+    dfs(1);
+    for (int i = 0, q, x, y; i < m; ++i) {
+        scanf("%d", &q);
+        if (q == 1) {
+            scanf("%d %d", &x, &y);
+            update(nodes[x][0], y);
+        } else {
+            scanf("%d", &x);
+            printf("%d\n", sum(nodes[x][1]) - sum(nodes[x][0] - 1));
+        }
+    }
+
+    close();
+    return 0;
+}
