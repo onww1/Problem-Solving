2f3461fd26de981d6d2f5366caed878f87fa809a
onww1 <onww1@naver.com>
2020-08-08 19:02:08 +0900

    boj1331 : 나이트 투어 (https://boj.kr/1331)

---
+++ BOJ/Simulation/boj1331_나이트_투어.cpp
@@ -0,0 +1,94 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+char in[5];
+bool ch[6][6];
+int pr, pc, ir, ic;
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf(" %s", in);
+    ir = pr = in[0] - 'A', ic = pc = in[1] - '1';
+    ch[pr][pc] = true;
+    bool valid = true;
+    for (int i = 1; i < 36; ++i) {
+        scanf(" %s", in);
+        int cr = in[0] - 'A';
+        int cc = in[1] - '1';
+        int dr = abs(cr - pr);
+        int dc = abs(cc - pc);
+        if (!((dr == 2 && dc == 1) || (dr == 1 && dc == 2)) || ch[cr][cc]) valid = false;
+        ch[cr][cc] = true;
+        pr = cr; pc = cc;
+    }
+    int dr = abs(ir - pr);
+    int dc = abs(ic - pc);
+    if (!((dr == 2 && dc == 1) || (dr == 1 && dc == 2))) valid = false;
+    puts(valid ? "Valid" : "Invalid");
+
+    close();
+    return 0;
+}
