1cb901dbd9a0cbe78141a7f143311d45bb562bf0
onww1 <onww1@naver.com>
2020-08-08 23:00:09 +0900

    boj1392 : 노래 악보 (https://boj.kr/1392)

---
+++ BOJ/Binary-search/boj1392_노래_악보.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e2 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, q, t, a[MAX];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d %d", &n, &q);
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", a + i);
+        a[i] += a[i - 1];
+    }
+    while (q--) {
+        scanf("%d", &t);
+        int lo = 1, hi = n;
+        while (lo <= hi) {
+            int mid = (lo + hi) >> 1;
+            if (a[mid] <= t) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        printf("%d\n", lo);
+    }
+
+    close();
+    return 0;
+}
