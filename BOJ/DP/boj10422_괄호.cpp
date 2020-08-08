a86399a9b8092add539190689a6fa5bd2115d9f4
onww1 <onww1@naver.com>
2020-08-08 15:01:11 +0900

    boj10422 : 괄호 (https://boj.kr/10422)

---
+++ BOJ/DP/boj10422_괄호.cpp
@@ -0,0 +1,87 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 5e3 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int t, l;
+ll dp[MAX][2];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    dp[2][0] = 1;
+    for (int i = 4; i < MAX; i += 2) {
+        dp[i][0] = (dp[i - 2][0] + dp[i - 2][1]) % MOD;
+        for (int k = 2; k <= i - 2; k += 2) 
+            dp[i][1] = (dp[i][1] + dp[k][0] * (dp[i - k][0] + dp[i - k][1])) % MOD;
+    }
+
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &l);
+        printf("%lld\n", (dp[l][0] + dp[l][1]) % MOD);
+    }
+
+    close();
+    return 0;
+}
