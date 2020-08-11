c92966c4030cf0d80ba3d041f6ec1daf524d884e
onww1 <onww1@naver.com>
2020-08-12 00:29:43 +0900

    boj1561 : 놀이 공원 (https://boj.kr/1561)

---
+++ BOJ/Binary-search/boj1561_놀이_공원.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, m, t[10005];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= m; ++i) scanf("%d", t + i);
+    ll lo = 0, hi = 60000000000LL;
+    while (lo <= hi) {
+        ll mid = (lo + hi) >> 1LL;
+        ll cnt = 0;
+        for (int i = 1; i <= m; ++i) cnt += mid / t[i] + 1;
+        if (cnt >= n) hi = mid - 1;
+        else lo = mid + 1;
+    }
+    vector<int> v;
+    for (int i = 1; i <= m; ++i) {
+        n -= lo / t[i] + 1;
+        if (lo % t[i] == 0) {
+            n += 1;
+            v.push_back(i);
+        }
+    }
+    printf("%d\n", v[n - 1]);
+
+    close();
+    return 0;
+}
