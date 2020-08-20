0a78dc174a28c2665e28e24e8119bd2402392251
onww1 <onww1@naver.com>
2020-08-21 06:21:03 +0900

    boj15900 : 나무 탈출 (https://boj.kr/15900)

---
+++ BOJ/Tree/boj15900_나무_탈출.cpp
@@ -0,0 +1,95 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 5e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, cnt;
+vector<int> edges[MAX];
+
+void dfs(int node, int pa, int depth) {
+    if (edges[node].size() == 1) {
+        cnt += depth;
+        return;
+    }
+    for (int next : edges[node]) {
+        if (next == pa) continue;
+        dfs(next, node, depth + 1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &n);
+    for (int i = 1, u, v; i < n; ++i) {
+        scanf("%d %d", &u, &v);
+        edges[u].push_back(v);
+        edges[v].push_back(u);
+    }
+    edges[1].push_back(0);
+    dfs(1, 0, 0);
+    puts((cnt & 1) ? "Yes" : "No");
+
+    close();
+    return 0;
+}
