56dbce08e73f8319570cdfb409f4a09310e0cf35
onww1 <onww1@naver.com>
2020-08-21 07:15:36 +0900

    boj9489 : 사촌 (https://boj.kkr/9489)

---
+++ BOJ/Tree/boj9489_사촌.cpp
@@ -0,0 +1,90 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int NMAX = 1e3 + 1;
+const int VMAX = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, k, a[NMAX], pa[VMAX];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    while (1) {
+        scanf("%d %d", &n, &k);
+        if (!n && !k) break;
+        int pidx = a[0] = -1;
+        for (int i = 1; i <= n; ++i) {
+            scanf("%d", a + i);
+            if (a[i - 1] + 1 != a[i]) pidx++;
+            pa[a[i]] = a[pidx];
+        }
+        int p = pa[k], pp = pa[p], ans = 0;
+        for (int i = 1; i <= n; ++i) 
+            if (pa[a[i]] != p && pa[pa[a[i]]] == pp) 
+                ans++;
+        printf("%d\n", ans);
+    }
+
+    close();
+    return 0;
+}
