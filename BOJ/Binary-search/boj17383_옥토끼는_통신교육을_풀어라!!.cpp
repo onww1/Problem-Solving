845d44b43a63349cf92f51e451ec1e79fbde2f09
onww1 <onww1@naver.com>
2020-08-28 00:44:18 +0900

    boj17383 : 옥토끼는 통신교육을 풀어라!! (https://boj.kr/17383)

---
+++ BOJ/Binary-search/boj17383_옥토끼는_통신교육을_풀어라!!.cpp
@@ -0,0 +1,86 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, t[MAX];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) scanf("%d", t + i);
+    int lo = 1, hi = 1e9;
+    while (lo <= hi) {
+        int mid = (lo + hi) >> 1;
+        ll sum = 0;
+        for (int i = 1; i <= n; ++i) sum += (t[i] + mid - 1) / mid;
+        sum -= 2 * n - 1;
+        if (sum <= 0) hi = mid - 1;
+        else lo = mid + 1;
+    }
+    printf("%d\n", lo);
+
+    close();
+    return 0;
+}
