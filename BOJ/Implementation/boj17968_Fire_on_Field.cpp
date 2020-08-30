1934c261f7a3c7c96ca4f6c6437604d70174ac9f
onww1 <onww1@naver.com>
2020-08-30 09:06:53 +0900

    boj17968 : Fire on Field (https://boj.kr/17968)

---
+++ BOJ/Implementation/boj17968_Fire_on_Field.cpp
@@ -0,0 +1,90 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e3 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, p[MAX], v[MAX];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &n);
+    p[0] = p[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 1; i - 2 * j >= 0; ++j) {
+            int pp = 2 * p[i - j] - p[i - 2 * j];
+            if (pp < 0 || pp >= MAX) continue;
+            v[pp] = i;
+        }
+        for (int j = 1; j <= n; ++j) {
+            if (v[j] != i) {
+                p[i] = j;
+                break;
+            }
+        }
+    }
+    printf("%d\n", p[n]);
+
+    close();
+    return 0;
+}
