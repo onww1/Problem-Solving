7c7799e930a1be6213e381d39e9d4e88c481fc97
onww1 <onww1@naver.com>
2020-09-19 00:05:36 +0900

    boj6764 : Sounds fishy! (https://boj.kr/6764)

---
+++ BOJ/Implementation/boj6764_Sounds_fishy!.cpp
@@ -0,0 +1,91 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+vector<int> v;
+
+int main(int argc, char *argv[]) {
+    open();
+
+    int n = 4;
+    for (int i = 0, a; i < n; ++i) {
+        scanf("%d", &a);
+        v.push_back(a);
+    }
+
+    bool asc = true, desc = true, cons = true;
+    for (int i = 1; i < n; ++i) {
+        asc = asc && (v[i] > v[i - 1]);
+        desc = desc && (v[i] < v[i - 1]);
+        cons = cons && (v[i] == v[i - 1]);
+    }
+
+    if (asc) puts("Fish Rising");
+    else if (desc) puts("Fish Diving");
+    else if (cons) puts("Fish At Constant Depth");
+    else puts("No Fish");
+
+    close();
+    return 0;
+}
