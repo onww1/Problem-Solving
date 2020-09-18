d2cb10467453ebc892c2be0edcae53aeb3a2655b
onww1 <onww1@naver.com>
2020-09-19 00:30:11 +0900

    boj18291 : 비요뜨의 징검다리 건너기 (https://boj.kr/18291)

---
+++ BOJ/Math/boj18291_비요뜨의_징검다리_건너기.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int t, n;
+
+ll _pow(ll base, ll exp) {
+    ll ret = 1;
+    while (exp) {
+        if (exp & 1) ret = ret * base % MOD;
+        base = base * base % MOD;
+        exp >>= 1;
+    }
+    return ret;
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        printf("%lld\n", n == 1 ? 1 : _pow(2, n - 2));
+    }
+
+    close();
+    return 0;
+}
