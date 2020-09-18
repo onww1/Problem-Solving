0defc7347dfc6164936ce95fe25d6997a262a481
onww1 <onww1@naver.com>
2020-09-19 02:00:23 +0900

    boj1515 : 수 이어 쓰기 (https://boj.kr/1515)

---
+++ BOJ/Brute-force/boj1515_수_이어_쓰기.cpp
@@ -0,0 +1,95 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, len, idx;
+char in[3005];
+
+vector<char> decomp(int num) {
+    vector<char> v;
+    while (num) {
+        v.push_back(num % 10 + '0');
+        num /= 10;
+    }
+    return v;
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf(" %s", in);
+    len = strlen(in);
+    for (n = 1; 1; n++) {
+        vector<char> comp = decomp(n);
+        while (!comp.empty()) {
+            char c = comp.back(); comp.pop_back();
+            if (idx < len && c == in[idx]) idx++;
+        }
+        if (idx == len) break;
+    }
+    printf("%d\n", n);
+
+    close();
+    return 0;
+}
