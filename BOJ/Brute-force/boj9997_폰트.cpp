6ac2081086ce73e4d085a4d42acd0de347ed3579
onww1 <onww1@naver.com>
2020-09-26 10:08:53 +0900

    boj9997 : 폰트 (https://boj.kr/9997)

---
+++ BOJ/Brute-force/boj9997_폰트.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+ll ans;
+int n, p[25];
+char in[105];
+
+void dfs(int idx, int v) {
+    if (v == (1 << 26) - 1) {
+        ans += (1 << (n - idx));
+        return;
+    }
+    for (int i = idx; i < n; ++i)
+        dfs(i + 1, v | p[i]);
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        scanf(" %s", in);
+        for (int j = 0; in[j]; ++j)
+            p[i] |= (1 << (in[j] - 'a'));
+    }
+    dfs(0, 0);
+    printf("%lld\n", ans);
+
+    close();
+    return 0;
+}
