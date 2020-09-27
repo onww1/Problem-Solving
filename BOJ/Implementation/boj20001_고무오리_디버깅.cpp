6af0487b580ea1b152eb58f3dbe5893c668cc87b
onww1 <onww1@naver.com>
2020-09-27 22:08:24 +0900

    boj20001 : 고무오리 디버깅 (https://boj.kr/20001)

---
+++ BOJ/Implementation/boj20001_고무오리_디버깅.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int main(int argc, char *argv[]) {
+    open();
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    int cnt = 0;
+    getline(cin, s);
+    while (1) {
+        getline(cin, s);
+        if (s == "고무오리 디버깅 끝") break;
+        if (s == "문제") cnt++;
+        else if (s == "고무오리") {
+            if (!cnt) cnt += 2;
+            else cnt--;
+        }
+    }
+    if (cnt) cout << "힝구" << '\n';
+    else cout << "고무오리야 사랑해" << '\n';
+
+    close();
+    return 0;
+}
