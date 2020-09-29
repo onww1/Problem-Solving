63a08e99290da46d7b48739b94106827d2fa33aa
onww1 <onww1@naver.com>
2020-09-30 07:56:39 +0900

    boj10090 : Counting Inversions (https://boj.kr/10090)

---
+++ BOJ/Indexed-tree/boj10090_Counting_Inversions.cpp
@@ -0,0 +1,98 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, tree[MAX];
+
+void update(int node, int x) {
+    while (node <= n) {
+        tree[node] += x;
+        node += node & -node;
+    }
+}
+
+int sum(int node) {
+    int ret = 0;
+    while (node) {
+        ret += tree[node];
+        node -= node & -node;
+    }
+    return ret;
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d", &n);
+    ll ans = 0;
+    for (int i = 0, a; i < n; ++i) {
+        scanf("%d", &a);
+        ans += i - sum(a - 1);
+        update(a, 1);
+    }
+    printf("%lld\n", ans);
+
+    close();
+    return 0;
+}
