fa2fe8859cd627bc7caf3b31e7bb3422189f6656
onww1 <onww1@naver.com>
2020-11-01 23:45:22 +0900

    boj16888 : 루트 게임 (https://boj.kr/16888)

---
+++ BOJ/DP/boj16888_루트_게임.cpp
@@ -0,0 +1,88 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int t, n;
+bool dp[MAX];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    for (int i = 1; i <= 1000; i++) dp[i * i] = true;
+    for (int i = 2; i < MAX; i++) {
+        if (!dp[i]) {
+            for (int j = 1; i + j * j < MAX; j++) 
+                dp[i + j * j] = true;
+        }
+    }
+
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        puts(dp[n] ? "koosaga" : "cubelover");
+    }
+
+    close();
+    return 0;
+}
