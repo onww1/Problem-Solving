539f3131c221068f7bd20171f124447683bdaa4c
onww1 <onww1@naver.com>
2020-11-13 00:18:43 +0900

    boj1354 : 무한 수열 2 (https://boj.kr/1354)

---
+++ BOJ/DP/boj1354_무한_수열_2.cpp
@@ -0,0 +1,87 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e7 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+ll n, p, q, x, y, dp[MAX];
+
+ll solve(ll i) {
+    if (i <= 0) return 1LL;
+    if (i < MAX) return dp[i];
+    return solve(i / p - x) + solve(i / q - y);
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%lld %lld %lld %lld %lld", &n, &p, &q, &x, &y);
+
+    dp[0] = 1;
+    for (int i = 1; i < MAX; i++) 
+        dp[i] = dp[max(i / p - x, 0LL)] + dp[max(i / q - y, 0LL)];
+
+    printf("%lld\n", solve(n));
+
+    close();
+    return 0;
+}
