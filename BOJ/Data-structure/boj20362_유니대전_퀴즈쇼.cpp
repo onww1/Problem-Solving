d7fe67d8ee141c677993e3e49fdc105dc771250c
onww1 <onww1@naver.com>
2021-01-05 00:21:50 +0900

    boj20362 : 유니대전 퀴즈쇼 (https://boj.kr/20362)

---
+++ BOJ/Data-structure/boj20362_유니대전_퀴즈쇼.cpp
@@ -0,0 +1,86 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, ans;
+string anickname, nickname, answer;
+map<string, int> cnt;
+
+int main(int argc, char *argv[]) {
+    open();
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> anickname;
+    for (int i = 0; i < n; i++) {
+        cin >> nickname >> answer;
+        if (nickname == anickname) ans = cnt[answer];
+        cnt[answer]++;
+    }
+    cout << ans << '\n';
+
+    close();
+    return 0;
+}
