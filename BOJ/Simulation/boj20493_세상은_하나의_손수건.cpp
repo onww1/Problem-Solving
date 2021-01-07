575b38f4bd38431db78552ee56caea9a81deabcb
onww1 <onww1@naver.com>
2021-01-07 18:40:21 +0900

    boj20493 : 세상은 하나의 손수건 (https://boj.kr/20493)

---
+++ BOJ/Simulation/boj20493_세상은_하나의_손수건.cpp
@@ -0,0 +1,87 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_x[] = {1, 0, -1, 0, -1, -1, 1, 1};
+const int move_y[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n, t, ct, x, y, d, p;
+char cd[10];
+
+int main(int argc, char *argv[]) {
+    open();
+
+    scanf("%d %d", &n, &t);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %s", &p, cd);
+        x += move_x[d] * (p - ct);
+        y += move_y[d] * (p - ct);
+        ct = p;
+        if (cd[0] == 'r') d = (d + 1) % 4;
+        else d = (d + 3) % 4;
+    }
+    x += move_x[d] * (t - ct);
+    y += move_y[d] * (t - ct);
+    printf("%d %d\n", x, y);
+
+    close();
+    return 0;
+}
