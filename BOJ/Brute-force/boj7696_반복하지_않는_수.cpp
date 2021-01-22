9019d5dd04335f83377a08921602531c6e5f2350
onww1 <onww1@naver.com>
2021-01-22 23:07:46 +0900

    boj7696 : 반복하지 않는 수 (https://boj.kr/7696)

---
+++ BOJ/Brute-force/boj7696_반복하지_않는_수.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#define X first
+#define Y second
+
+#ifdef NON_SUBMIT
+#define TEST(n) (n)
+#else
+#define TEST(n) ((void)0)
+#endif
+
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <int, pii> piii;
+typedef tuple <int, int, int> ti3;
+
+clock_t start_time, end_time;
+
+void open() {
+    TEST(freopen("input.txt", "r", stdin));
+    TEST(freopen("output.txt", "w", stdout));
+    TEST(freopen("debug.txt", "w", stderr));
+    TEST(start_time = clock());
+}
+
+void close() {
+    TEST(end_time = clock());
+    TEST(printf("Total time : %Lf seconds\n", (long double)(end_time - start_time) / CLOCKS_PER_SEC));
+}
+
+const int MAX = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3fLL;
+const db PI = acos(-1);
+const ldb ERR = 1e-10;
+const int move_r[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int move_c[] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+int n;
+vector<int> v;
+
+void dfs(int idx, int state, int val) {
+    if (idx == 9) return;
+    v.push_back(val);
+    for (int i = idx ? 0 : 1; i < 10; i++) {
+        if (state & (1 << i)) continue;
+        dfs(idx + 1, state | (1 << i), val * 10 + i);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    open();
+
+    dfs(0, 0, 0);
+    sort(v.begin(), v.end());
+    v.resize(unique(v.begin(), v.end()) - v.begin());
+
+    while (1) {
+        scanf("%d", &n);
+        if (!n) break;
+        printf("%d\n", v[n]);
+    }
+
+    close();
+    return 0;
+}
