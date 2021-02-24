231e639cb2b0e98fc718e9720508e4fb74a7576a
onww1 <onww1@naver.com>
2021-02-24 21:54:48 +0900

    lc15 : 3Sum (https://leetcode.com/problems/3sum)

---
+++ LeetCode/Medium/lc15.cpp
@@ -0,0 +1,155 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ListNode* makeList(vector<int>& nodes);
+TreeNode* makeTree(vector<int>& nodes);
+bool matchList(ListNode* anode, ListNode* bnode);
+bool matchTree(TreeNode* anode, TreeNode* bnode);
+
+const int EMPTY = 0x7f7f7f7f;
+
+/* Solution Class */
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> answer;
+        int prevX = -100001;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0)
+                break;
+            if (nums[i] == prevX)
+                continue;
+            prevX = nums[i];
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    vector<int> triplet = {nums[i], nums[left], nums[right]};
+                    answer.push_back(triplet);  
+                    while (left < right && nums[left] == triplet[1]) left++;
+                    while (left < right && nums[right] == triplet[2]) right--; 
+                }
+                else if (sum > 0) right--;
+                else left++;
+            }
+        }
+        return answer;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    /* Input */
+    vector<vector<int>> nums = {
+        {-1, 0, 1, 2, -1, -4},
+        {},
+        {0}
+    };
+
+    /* Output */
+    vector<vector<vector<int>>> ret;
+    for (int i = 0; i < nums.size(); i++)
+        ret.push_back(solution.threeSum(nums[i]));
+
+    /* Answer */
+    vector<vector<vector<int>>> answer = {
+        {{-1, -1, 2}, {-1, 0, 1}},
+        {},
+        {}
+    };
+    
+    /* Check */
+    assert(ret == answer);
+
+    puts("PASS!");
+    return 0;
+}
+
+ListNode* makeList(vector<int>& nodes) {
+    if (nodes.empty())
+        return nullptr;
+
+    ListNode* head = new ListNode(nodes[0]);
+    ListNode* ptr = head;
+    for (int i = 1; i < nodes.size(); i++) {
+        ptr->next = new ListNode(nodes[i]);
+        ptr = ptr->next;
+    }
+
+    return head;
+}
+
+TreeNode* makeTree(vector<int>& nodes) {
+    if (nodes.empty())
+        return NULL;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> que;
+    que.push(root);
+
+    int idx = 1;
+    while (!que.empty() && idx < nodes.size()) {
+        TreeNode* node = que.front();
+        que.pop();
+
+        if (nodes[idx] != EMPTY) {
+            node->left = new TreeNode(nodes[idx]);
+            que.push(node->left);
+        }
+        idx++;
+
+        if (nodes[idx] != EMPTY) {
+            node->right = new TreeNode(nodes[idx]);
+            que.push(node->right);
+        }
+        idx++;
+    }
+
+    return root;
+}
+
+bool matchList(ListNode* anode, ListNode* bnode) {
+    while (anode && bnode) {
+        if (anode->val != bnode->val)
+            return false;
+        anode = anode->next;
+        bnode = bnode->next;
+    }
+    return !anode && !bnode;
+}
+
+bool matchTree(TreeNode* anode, TreeNode* bnode) {
+    if (!anode && !bnode) 
+        return true;
+    if (!anode || !bnode)
+        return false;
+    if (anode->val != bnode->val)
+        return false;
+    return matchTree(anode->left, bnode->left) && matchTree(anode->right, bnode->right);
+}
