713f465ea415fdaa46a2f472727db5ad70fe13c4
onww1 <onww1@naver.com>
2021-02-25 12:46:30 +0900

    lc17 : Letter Combinations of a Phone Number (https://leetcode.com/problems/letter-combinations-of-a-phone-number)

---
+++ LeetCode/Medium/lc17.cpp
@@ -0,0 +1,166 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ListNode* makeList(vector<int>& nodes);
+TreeNode* makeTree(vector<int>& nodes);
+bool matchList(ListNode* anode, ListNode* bnode);
+bool matchTree(TreeNode* anode, TreeNode* bnode);
+
+const int EMPTY = 0x7f7f7f7f;
+
+/* Solution Class */
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+        vector<string> answer;
+        solve(0, "", digits, answer);
+        return answer;
+    }
+
+private:
+    void solve(int idx, string cur, string& digits, vector<string>& answer) {
+        if (idx == digits.length()) {
+            answer.emplace_back(cur);
+            return;
+        }
+
+        vector<char> chars = getCharacters(digits[idx]);
+        for (char c: chars)
+            solve(idx + 1, cur + c, digits, answer);
+    }
+
+    vector<char> getCharacters(char num) {
+        switch (num) {
+            case '2':
+                return {'a', 'b', 'c'};
+            case '3':
+                return {'d', 'e', 'f'};
+            case '4':
+                return {'g', 'h', 'i'};
+            case '5':
+                return {'j', 'k', 'l'};
+            case '6':
+                return {'m', 'n', 'o'};
+            case '7':
+                return {'p', 'q', 'r', 's'};
+            case '8':
+                return {'t', 'u', 'v'};
+            case '9':
+                return {'w', 'x', 'y', 'z'};
+        }
+        return {};
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    /* Input */
+    vector<string> digits = {"23", "", "2"};
+
+    /* Output */
+    vector<vector<string>> ret;
+    for (int i = 0; i < digits.size(); i++)
+        ret.push_back(solution.letterCombinations(digits[i]));
+
+    /* Answer */
+    vector<vector<string>> answer = {
+        {"ad","ae","af","bd","be","bf","cd","ce","cf"},
+        {},
+        {"a", "b", "c"}
+    };
+
+    /* Check */
+    assert(ret == answer);
+
+    puts("PASS!");
+    return 0;
+}
+
+ListNode* makeList(vector<int>& nodes) {
+    if (nodes.empty())
+        return nullptr;
+
+    ListNode* head = new ListNode(nodes[0]);
+    ListNode* ptr = head;
+    for (int i = 1; i < nodes.size(); i++) {
+        ptr->next = new ListNode(nodes[i]);
+        ptr = ptr->next;
+    }
+
+    return head;
+}
+
+TreeNode* makeTree(vector<int>& nodes) {
+    if (nodes.empty())
+        return NULL;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> que;
+    que.push(root);
+
+    int idx = 1;
+    while (!que.empty() && idx < nodes.size()) {
+        TreeNode* node = que.front();
+        que.pop();
+
+        if (nodes[idx] != EMPTY) {
+            node->left = new TreeNode(nodes[idx]);
+            que.push(node->left);
+        }
+        idx++;
+
+        if (nodes[idx] != EMPTY) {
+            node->right = new TreeNode(nodes[idx]);
+            que.push(node->right);
+        }
+        idx++;
+    }
+
+    return root;
+}
+
+bool matchList(ListNode* anode, ListNode* bnode) {
+    while (anode && bnode) {
+        if (anode->val != bnode->val)
+            return false;
+        anode = anode->next;
+        bnode = bnode->next;
+    }
+    return !anode && !bnode;
+}
+
+bool matchTree(TreeNode* anode, TreeNode* bnode) {
+    if (!anode && !bnode) 
+        return true;
+    if (!anode || !bnode)
+        return false;
+    if (anode->val != bnode->val)
+        return false;
+    return matchTree(anode->left, bnode->left) && matchTree(anode->right, bnode->right);
+}
