45399a87cf6ba75b0804885d91516e4ab6bfb0fe
onww1 <onww1@naver.com>
2021-03-01 00:49:57 +0900

    lc767 : Reorganize String (https://leetcode.com/problems/reorganize-string)

---
+++ LeetCode/Medium/lc767.cpp
@@ -0,0 +1,148 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ListNode* makeList(vector<int>& nodes);
+TreeNode* makeTree(vector<int>& nodes);
+bool matchList(ListNode* anode, ListNode* bnode);
+bool matchTree(TreeNode* anode, TreeNode* bnode);
+
+const int EMPTY = 0x7f7f7f7f;
+
+/* Solution Class */
+class Solution {
+public:
+    string reorganizeString(string S) {
+        vector<pair<int,char>> count(26, {0, 'a'});
+        for (char c: S)
+            count[c - 'a'].first++;
+        for (int i = 0; i < 26; i++)
+            count[i].second = 'a' + i;
+        sort(count.begin(), count.end(), greater<pair<int,char>>());
+
+        if (count[0].first > (S.length() + 1) / 2)
+            return "";
+
+        vector<char> answer(S.length());
+        int idx = 0;
+        for (int i = 0; i < S.length(); i += 2) {
+            if (count[idx].first == 0)
+                idx++;
+            answer[i] = count[idx].second;
+            count[idx].first--;
+        }
+        for (int i = 1; i < S.length(); i += 2) {
+            if (count[idx].first == 0)
+                idx++;
+            answer[i] = count[idx].second;
+            count[idx].first--;
+        }
+        return string(answer.begin(), answer.end());
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    /* Input */
+    vector<string> S = {"aab", "aaab"};
+
+    /* Output */
+    vector<string> ret;
+    for (int i = 0; i < S.size(); i++)
+        ret.push_back(solution.reorganizeString(S[i]));
+
+    /* Answer */
+    vector<string> answer = {"aba", "aaab"};
+
+    /* Check */
+    assert(ret == answer);
+
+    puts("PASS!");
+    return 0;
+}
+
+ListNode* makeList(vector<int>& nodes) {
+    if (nodes.empty())
+        return nullptr;
+
+    ListNode* head = new ListNode(nodes[0]);
+    ListNode* ptr = head;
+    for (int i = 1; i < nodes.size(); i++) {
+        ptr->next = new ListNode(nodes[i]);
+        ptr = ptr->next;
+    }
+
+    return head;
+}
+
+TreeNode* makeTree(vector<int>& nodes) {
+    if (nodes.empty())
+        return NULL;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> que;
+    que.push(root);
+
+    int idx = 1;
+    while (!que.empty() && idx < nodes.size()) {
+        TreeNode* node = que.front();
+        que.pop();
+
+        if (nodes[idx] != EMPTY) {
+            node->left = new TreeNode(nodes[idx]);
+            que.push(node->left);
+        }
+        idx++;
+
+        if (nodes[idx] != EMPTY) {
+            node->right = new TreeNode(nodes[idx]);
+            que.push(node->right);
+        }
+        idx++;
+    }
+
+    return root;
+}
+
+bool matchList(ListNode* anode, ListNode* bnode) {
+    while (anode && bnode) {
+        if (anode->val != bnode->val)
+            return false;
+        anode = anode->next;
+        bnode = bnode->next;
+    }
+    return !anode && !bnode;
+}
+
+bool matchTree(TreeNode* anode, TreeNode* bnode) {
+    if (!anode && !bnode) 
+        return true;
+    if (!anode || !bnode)
+        return false;
+    if (anode->val != bnode->val)
+        return false;
+    return matchTree(anode->left, bnode->left) && matchTree(anode->right, bnode->right);
+}
