27147c2f792556f9462b8695df89c10dc7d98e15
onww1 <onww1@naver.com>
2021-03-12 23:51:00 +0900

    lc45 : Jump Game II (https://leetcode.com/problems/jump-game-ii)

---
+++ LeetCode/Medium/lc45.cpp
@@ -0,0 +1,133 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ListNode* makeList(vector<int>& nodes);
+TreeNode* makeTree(vector<int>& nodes);
+bool matchList(ListNode* anode, ListNode* bnode);
+bool matchTree(TreeNode* anode, TreeNode* bnode);
+
+const int EMPTY = 0x7f7f7f7f;
+
+/* Solution Class */
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> minJump(n, n);
+        minJump[0] = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 1; i + j < n && j <= nums[i]; j++)
+                minJump[i + j] = min(minJump[i + j], minJump[i] + 1);
+        return minJump[n - 1];
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    /* Input */
+    vector<vector<int>> numsVec = {
+        {2, 3, 1, 1, 4},
+        {2, 3, 0, 1, 4}
+    };
+
+    /* Output */
+    vector<int> ret;
+    for (vector<int>& nums: numsVec)
+        ret.push_back(solution.jump(nums));
+
+    /* Answer */
+    vector<int> answer = {2, 2};
+    
+    /* Check */
+    assert(ret == answer);
+
+    puts("PASS!");
+    return 0;
+}
+
+ListNode* makeList(vector<int>& nodes) {
+    if (nodes.empty())
+        return nullptr;
+
+    ListNode* head = new ListNode(nodes[0]);
+    ListNode* ptr = head;
+    for (int i = 1; i < nodes.size(); i++) {
+        ptr->next = new ListNode(nodes[i]);
+        ptr = ptr->next;
+    }
+
+    return head;
+}
+
+TreeNode* makeTree(vector<int>& nodes) {
+    if (nodes.empty())
+        return NULL;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> que;
+    que.push(root);
+
+    int idx = 1;
+    while (!que.empty() && idx < nodes.size()) {
+        TreeNode* node = que.front();
+        que.pop();
+
+        if (nodes[idx] != EMPTY) {
+            node->left = new TreeNode(nodes[idx]);
+            que.push(node->left);
+        }
+        idx++;
+
+        if (nodes[idx] != EMPTY) {
+            node->right = new TreeNode(nodes[idx]);
+            que.push(node->right);
+        }
+        idx++;
+    }
+
+    return root;
+}
+
+bool matchList(ListNode* anode, ListNode* bnode) {
+    while (anode && bnode) {
+        if (anode->val != bnode->val)
+            return false;
+        anode = anode->next;
+        bnode = bnode->next;
+    }
+    return !anode && !bnode;
+}
+
+bool matchTree(TreeNode* anode, TreeNode* bnode) {
+    if (!anode && !bnode) 
+        return true;
+    if (!anode || !bnode)
+        return false;
+    if (anode->val != bnode->val)
+        return false;
+    return matchTree(anode->left, bnode->left) && matchTree(anode->right, bnode->right);
+}
