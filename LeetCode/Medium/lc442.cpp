44c9c410c82d248869ddf7bb35f7e1c581dc6f64
onww1 <onww1@naver.com>
2021-03-20 20:05:31 +0900

    lc442 : Find All Duplicates in an Array (https://leetcode.com/problems/find-all-duplicates-in-an-array)

---
+++ LeetCode/Medium/lc442.cpp
@@ -0,0 +1,151 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ListNode* makeList(vector<int>& nodes);
+TreeNode* makeTree(vector<int>& nodes);
+bool matchList(ListNode* anode, ListNode* bnode);
+bool matchTree(TreeNode* anode, TreeNode* bnode);
+
+const int EMPTY = 0x7f7f7f7f;
+
+/* Solution Class */
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            while (nums[i] != i + 1) {
+                if (nums[nums[i] - 1] == nums[i]) {
+                    result.push_back(nums[i]);
+                    break;
+                }
+                int idx = nums[i] - 1;
+                swap(nums[i], nums[idx]);
+                if (idx < i)
+                    break;
+            }
+        }
+        return result;
+    }
+
+    vector<int> findDuplicatesV2(vector<int>& nums) {
+        vector<int> result;
+        for (int num: nums) {
+            if (nums[abs(num) - 1] < 0)
+                result.push_back(abs(num));
+            nums[abs(num) - 1] *= -1;
+        }
+        return result;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    /* Input */
+    vector<vector<int>> numsVec = {
+        {4, 3, 2, 7, 8, 2, 3, 1}
+    };
+
+    /* Output */
+    vector<vector<int>> ret;
+    for (vector<int>& nums: numsVec)
+        ret.push_back(solution.findDuplicates(nums));
+
+    /* Answer */
+    vector<vector<int>> answer = {
+        {2, 3}
+    };
+    
+    /* Check */
+    assert(ret == answer);
+
+    puts("PASS!");
+    return 0;
+}
+
+ListNode* makeList(vector<int>& nodes) {
+    if (nodes.empty())
+        return nullptr;
+
+    ListNode* head = new ListNode(nodes[0]);
+    ListNode* ptr = head;
+    for (int i = 1; i < nodes.size(); i++) {
+        ptr->next = new ListNode(nodes[i]);
+        ptr = ptr->next;
+    }
+
+    return head;
+}
+
+TreeNode* makeTree(vector<int>& nodes) {
+    if (nodes.empty())
+        return NULL;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> que;
+    que.push(root);
+
+    int idx = 1;
+    while (!que.empty() && idx < nodes.size()) {
+        TreeNode* node = que.front();
+        que.pop();
+
+        if (nodes[idx] != EMPTY) {
+            node->left = new TreeNode(nodes[idx]);
+            que.push(node->left);
+        }
+        idx++;
+
+        if (nodes[idx] != EMPTY) {
+            node->right = new TreeNode(nodes[idx]);
+            que.push(node->right);
+        }
+        idx++;
+    }
+
+    return root;
+}
+
+bool matchList(ListNode* anode, ListNode* bnode) {
+    while (anode && bnode) {
+        if (anode->val != bnode->val)
+            return false;
+        anode = anode->next;
+        bnode = bnode->next;
+    }
+    return !anode && !bnode;
+}
+
+bool matchTree(TreeNode* anode, TreeNode* bnode) {
+    if (!anode && !bnode) 
+        return true;
+    if (!anode || !bnode)
+        return false;
+    if (anode->val != bnode->val)
+        return false;
+    return matchTree(anode->left, bnode->left) && matchTree(anode->right, bnode->right);
+}
