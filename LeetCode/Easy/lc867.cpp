ac9a989d8d60759a0f8a5153f0988d4892f9a01e
onww1 <onww1@naver.com>
2021-03-24 19:49:27 +0900

    lc867 : Transpose Matrix (https://leetcode.com/problems/transpose-matrix)

---
+++ LeetCode/Easy/lc867.cpp
@@ -0,0 +1,136 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ListNode* makeList(vector<int>& nodes);
+TreeNode* makeTree(vector<int>& nodes);
+bool matchList(ListNode* anode, ListNode* bnode);
+bool matchTree(TreeNode* anode, TreeNode* bnode);
+
+const int EMPTY = 0x7f7f7f7f;
+
+/* Solution Class */
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> transposed_matrix(m, vector<int>(n, 0));
+        for (int row = 0; row < n; row++)
+            for (int col = 0; col < m; col++)
+                transposed_matrix[col][row] = matrix[row][col];
+        return transposed_matrix;   
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    /* Input */
+    vector<vector<vector<int>>> matrixVec = {
+        {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+        {{1, 2, 3}, {4, 5, 6}}
+    };
+
+    /* Output */
+    vector<vector<vector<int>>> ret;
+    for (vector<vector<int>>& matrix: matrixVec)
+        ret.push_back(solution.transpose(matrix));
+
+    /* Answer */
+    vector<vector<vector<int>>> answer = {
+        {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}},
+        {{1, 4}, {2, 5}, {3, 6}}
+    };
+
+    /* Check */
+    assert(ret == answer);
+
+    puts("PASS!");
+    return 0;
+}
+
+ListNode* makeList(vector<int>& nodes) {
+    if (nodes.empty())
+        return nullptr;
+
+    ListNode* head = new ListNode(nodes[0]);
+    ListNode* ptr = head;
+    for (int i = 1; i < nodes.size(); i++) {
+        ptr->next = new ListNode(nodes[i]);
+        ptr = ptr->next;
+    }
+
+    return head;
+}
+
+TreeNode* makeTree(vector<int>& nodes) {
+    if (nodes.empty())
+        return NULL;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> que;
+    que.push(root);
+
+    int idx = 1;
+    while (!que.empty() && idx < nodes.size()) {
+        TreeNode* node = que.front();
+        que.pop();
+
+        if (nodes[idx] != EMPTY) {
+            node->left = new TreeNode(nodes[idx]);
+            que.push(node->left);
+        }
+        idx++;
+
+        if (nodes[idx] != EMPTY) {
+            node->right = new TreeNode(nodes[idx]);
+            que.push(node->right);
+        }
+        idx++;
+    }
+
+    return root;
+}
+
+bool matchList(ListNode* anode, ListNode* bnode) {
+    while (anode && bnode) {
+        if (anode->val != bnode->val)
+            return false;
+        anode = anode->next;
+        bnode = bnode->next;
+    }
+    return !anode && !bnode;
+}
+
+bool matchTree(TreeNode* anode, TreeNode* bnode) {
+    if (!anode && !bnode) 
+        return true;
+    if (!anode || !bnode)
+        return false;
+    if (anode->val != bnode->val)
+        return false;
+    return matchTree(anode->left, bnode->left) && matchTree(anode->right, bnode->right);
+}
