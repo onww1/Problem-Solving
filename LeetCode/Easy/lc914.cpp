25fc932c43994295d0983ec4eecccaa8050944c6
onww1 <onww1@naver.com>
2021-04-18 23:57:52 +0900

    lc914 : X of a Kind in a Deck of Cards (https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards)

---
+++ LeetCode/Easy/lc914.cpp
@@ -0,0 +1,142 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ListNode* makeList(vector<int>& nodes);
+TreeNode* makeTree(vector<int>& nodes);
+bool matchList(ListNode* anode, ListNode* bnode);
+bool matchTree(TreeNode* anode, TreeNode* bnode);
+
+const int EMPTY = 0x7f7f7f7f;
+
+/* Solution Class */
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        unordered_map<int,int> count;
+        for (int card: deck)
+            count[card]++;
+        int GCD = 0;
+        for (auto it = count.begin(); it != count.end(); it++)
+            GCD = gcd(GCD, it->second);
+        return GCD >= 2;
+    }
+
+private:
+    int gcd(int a, int b) {
+        if (!b) return a;
+        return gcd(b, a % b);
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    /* Input */
+    vector<vector<int>> deckVec = {
+        {1, 2, 3, 4, 4, 3, 2, 1},
+        {1, 1, 1, 2, 2, 2, 3, 3},
+        {1},
+        {1, 1},
+        {1, 1, 2, 2, 2, 2}
+    };
+
+    /* Output */
+    vector<bool> ret;
+    for (vector<int>& deck: deckVec)
+        ret.push_back(solution.hasGroupsSizeX(deck));
+
+    /* Answer */
+    vector<bool> answer = {true, false, false, true, true};
+
+    /* Check */
+    assert(ret == answer);
+
+    puts("PASS!");
+    return 0;
+}
+
+ListNode* makeList(vector<int>& nodes) {
+    if (nodes.empty())
+        return nullptr;
+
+    ListNode* head = new ListNode(nodes[0]);
+    ListNode* ptr = head;
+    for (int i = 1; i < nodes.size(); i++) {
+        ptr->next = new ListNode(nodes[i]);
+        ptr = ptr->next;
+    }
+
+    return head;
+}
+
+TreeNode* makeTree(vector<int>& nodes) {
+    if (nodes.empty())
+        return NULL;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> que;
+    que.push(root);
+
+    int idx = 1;
+    while (!que.empty() && idx < nodes.size()) {
+        TreeNode* node = que.front();
+        que.pop();
+
+        if (nodes[idx] != EMPTY) {
+            node->left = new TreeNode(nodes[idx]);
+            que.push(node->left);
+        }
+        idx++;
+
+        if (nodes[idx] != EMPTY) {
+            node->right = new TreeNode(nodes[idx]);
+            que.push(node->right);
+        }
+        idx++;
+    }
+
+    return root;
+}
+
+bool matchList(ListNode* anode, ListNode* bnode) {
+    while (anode && bnode) {
+        if (anode->val != bnode->val)
+            return false;
+        anode = anode->next;
+        bnode = bnode->next;
+    }
+    return !anode && !bnode;
+}
+
+bool matchTree(TreeNode* anode, TreeNode* bnode) {
+    if (!anode && !bnode) 
+        return true;
+    if (!anode || !bnode)
+        return false;
+    if (anode->val != bnode->val)
+        return false;
+    return matchTree(anode->left, bnode->left) && matchTree(anode->right, bnode->right);
+}
